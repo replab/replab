bd119ae48b7cf954d1762183db98462de41411f7
Denis Rosset <physics@denisrosset.com>
2018-12-13 16:42:56 +0100

    Completed permutation helper methods + tests.

--- +replab/Perm.m
+++ +replab/Perm.m
@@ -1,49 +1,10 @@
 classdef Perm
     
-    methods (Static)
-        
-        function mat = matrix(perm)
-        % Returns the permutation matrix corresponding to the given permutation
-        % such that matrix multiplication is compatible with composition of
-        % permutations, i.e. 
-        % Perm.matrix(Perm.compose(x, y)) = Perm.matrix(x) * Perm.matrix(y)
-            n = length(perm);
-            mat = sparse(perm, 1:n, ones(1, n), n, n);
-        end
-        
-        function z = compose(x, y)
-        % Returns the composition of x and y
-            assert(length(x) == length(y));
-            z = x(y);
-        end
-        
-        function b = isIdentity(perm)
-        % Tests whether the given permutation is the identity
-            n = length(perm);
-            b = true;
-            for i = 1:n
-                if perm(i) ~= i
-                    b = false;
-                    return
-                end
-            end
-        end
+    methods (Static) % CONSTRUCTION METHODS
         
         function x = identity(n)
         % Returns the identity permutation
             x = 1:n;
-        end
-        
-        function y = inverse(x)
-        % Returns the permutation y such that Perm.compose(x, y) == identity
-            n = length(x);
-            y = zeros(1, n);
-            y(x) = 1:n;
-        end
-        
-        function x = random(n)
-        % Returns a random permutation acting on 1..n
-            x = randperm(n);
         end
         
         function p = fromCycles(n, varargin)
@@ -60,6 +21,83 @@ classdef Perm
                 newEl(cycle) = cycleImage;
                 p = Perm.compose(newEl, p);
             end
+        end
+        
+        function x = random(n)
+        % Returns a random permutation acting on 1..n
+            x = randperm(n);
+        end
+        
+    end
+    
+    methods (Static) % PROPERTIES
+        
+            function b = isIdentity(perm)
+        % Tests whether the given permutation is the identity
+            n = length(perm);
+            b = true;
+            for i = 1:n
+                if perm(i) ~= i
+                    b = false;
+                    return
+                end
+            end
+        end
+
+    end
+    
+    methods (Static) % TRANSFORMATIONS
+        
+        
+        function z = compose(x, y)
+        % Returns the composition of x and y
+            assert(length(x) == length(y));
+            z = x(y);
+        end
+        
+        function y = inverse(x)
+        % Returns the permutation y such that Perm.compose(x, y) == identity
+            n = length(x);
+            y = zeros(1, n);
+            y(x) = 1:n;
+        end
+        
+    end
+
+    methods (Static) % REPRESENTATIONS
+        
+        function mat = matrix(perm)
+        % Returns the permutation matrix corresponding to the given permutation
+        % such that matrix multiplication is compatible with composition of
+        % permutations, i.e. 
+        % Perm.matrix(Perm.compose(x, y)) = Perm.matrix(x) * Perm.matrix(y)
+            n = length(perm);
+            mat = sparse(perm, 1:n, ones(1, n), n, n);
+        end
+        
+    end
+        
+    methods (Static) % ACTIONS
+        
+        function i = image(perm, p)
+        % Returns the image i of the point p under the permutation perm
+            i = perm(p);
+        end
+        
+        function vec = vectorAction(perm, vec)
+        % Permutation of a column vector
+        % equivalent to Perm.matrix(perm) * vec
+            assert(length(perm) == length(vec));
+            vec(perm) = vec;
+        end
+        
+        function M = matrixAction(perm, M)
+        % Returns the image under the action of perm on the columns and rows of M
+        % i.e. Perm.matrix(perm)*M*Perm.matrix(perm)'
+            n = length(perm);
+            assert(size(M, 1) == n);
+            assert(size(M, 2) == n);
+            M(perm, perm) = M;
         end
         
     end
--- tests/PermTest.m
+++ tests/PermTest.m
@@ -31,9 +31,72 @@ function test_compose()
     end
 end
 
-function test_isIdentity()
+function test_isIdentity_identity()
     import replab.*
     n = 100;
     assert(Perm.isIdentity(Perm.identity(100)));
     assert(~Perm.isIdentity([1 3 2 4 5]));
+end
+
+function test_inverse()
+    import replab.*
+    n = 100;
+    for i = 1:100
+        x = Perm.random(n);
+        y = Perm.random(n);
+        assertEqual(Perm.identity(n), Perm.compose(x, Perm.inverse(x)));
+        xy = Perm.compose(x, y);
+        yIxI = Perm.compose(Perm.inverse(y), Perm.inverse(x));
+        assertEqual(Perm.inverse(xy), yIxI);
+    end
+end
+
+function test_image()
+    import replab.*
+    n = 100;
+    for i = 1:100
+        x = Perm.random(n);
+        y = Perm.random(n);
+        z = Perm.compose(x, y);
+        p = randi(n);
+        imgy = Perm.image(y, p);
+        imgxy = Perm.image(x, imgy);
+        imgz = Perm.image(z, p);
+        assertEqual(imgxy, imgz);
+    end
+end
+
+function test_vectorAction()
+% test compatibility of action with group
+    import replab.*
+    n = 100;
+    for i = 1:100
+        v = rand(n, 1);
+        x = Perm.random(n);
+        y = Perm.random(n);
+        z = Perm.compose(x, y);
+        imgy = Perm.vectorAction(y, v);
+        imgxy = Perm.vectorAction(x, imgy);
+        imgz = Perm.vectorAction(z, v);
+        assertEqual(imgz, imgxy);
+        Mz = Perm.matrix(z);
+        assertEqual(Mz * v, imgz);
+    end
+end
+
+function test_matrixAction()
+    import replab.*
+    n = 10;
+    for i = 1:100
+        M = rand(n, n);
+        x = Perm.random(n);
+        y = Perm.random(n);
+        z = Perm.compose(x, y);
+        imgy = Perm.matrixAction(y, M);
+        imgxy = Perm.matrixAction(x, imgy);
+        imgz = Perm.matrixAction(z, M);
+        assertEqual(imgz, imgxy);
+        rhoz = Perm.matrix(z);
+        assertEqual(rhoz * M * rhoz', imgz);
+    end
 end
