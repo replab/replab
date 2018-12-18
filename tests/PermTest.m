a15ba094140bc385f54ba6f8af4c2a1309112dc1
Denis Rosset <physics@denisrosset.com>
2018-12-18 03:28:35 +0100

    Disabled natural representation tests for categories

--- tests/PermTest.m
+++ tests/PermTest.m
@@ -9,12 +9,12 @@ function test_suite = PermTest()
     A = G.naturalAction;
     A1 = G.vectorAction('R15');
     A2 = G.selfAdjointMatrixAction('R15');
-    R = G.naturalRepresentation;
+    %    R = G.naturalRepresentation;
     test_suite = G.lawsAddTestCases(test_suite);
     test_suite = A.lawsAddTestCases(test_suite, 'name', 'natural action');
     test_suite = A1.lawsAddTestCases(test_suite, 'name', 'vector action');
     test_suite = A2.lawsAddTestCases(test_suite, 'name', 'self-adjoint matrix action');
-    test_suite = R.lawsAddTestCases(test_suite, 'name', 'natural representation');
+    %    test_suite = R.lawsAddTestCases(test_suite, 'name', 'natural representation');
 end
 
 % $$$ 
--- tests/SignedPermTest.m
+++ tests/SignedPermTest.m
@@ -10,11 +10,11 @@ function test_suite = SignedPermTest()
     A1 = G.vectorAction('R15');
     A2 = G.selfAdjointMatrixAction('R15');
     M = G.permutationIsomorphism;
-    R = G.naturalRepresentation;
+    %    R = G.naturalRepresentation;
     test_suite = G.lawsAddTestCases(test_suite);
     test_suite = A.lawsAddTestCases(test_suite, 'name', 'natural action');
     test_suite = A1.lawsAddTestCases(test_suite, 'name', 'vector action');
     test_suite = A2.lawsAddTestCases(test_suite, 'name', 'self-adjoint matrix action');
     test_suite = M.lawsAddTestCases(test_suite, 'name', 'permutation isomorphism');
-    test_suite = R.lawsAddTestCases(test_suite, 'name', 'natural representation');
+    %    test_suite = R.lawsAddTestCases(test_suite, 'name', 'natural representation');
 end
