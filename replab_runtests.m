988986bfe9534bdeefec8bb6fff571882b36ea9d
jdbancal <jdbancal@gmail.com>
2018-12-20 17:34:34 +0100

    added script documentation

--- replab_runtests.m
+++ replab_runtests.m
@@ -1,4 +1,6 @@
 function result = replab_runtests
+    % Tests the library functionalities
+    
     initialPath = pwd;
     [pathStr, name, extension] = fileparts(which(mfilename));
     cd(pathStr)
