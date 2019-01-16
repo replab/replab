13383455d3d8de4c9fc1cf5db69c0eb0868762c3
Denis Rosset <physics@denisrosset.com>
2019-01-16 20:29:26 +0100

    Added automatic release numbering.

---
+++ replab_release.m
@@ -0,0 +1,66 @@
+function replab_release
+% Automates the release of a RepLAB version
+    [pathStr, name, extension] = fileparts(which(mfilename)); % Verifies current directory
+    assert(isequal(pathStr, pwd), 'replab_release must be run from the RepLAB folder');
+    % Verifies that repository is clean
+    [status, cmdout] = system('git diff --exit-code');
+    assert(status == 0, 'The repository has local unstaged changes.');
+    [status, cmdout] = system('git diff --cached --exit-code');
+    assert(status == 0, 'The repository has staged but uncommitted changes.');
+    [status, masterRef] = system('git rev-parse --verify master');
+    [status, originMasterRef] = system('git rev-parse --verify origin/master');
+    assert(isequal(masterRef, originMasterRef), 'The repository has committed changes that have not been pushed.');
+    [major minor patch snapshot txt] = replab_version;
+    userTxt = input(sprintf('Release version [%s]:\n', txt), 's');
+    if ~isempty(userTxt)
+        [major minor patch snapshot txt] = replab_version(userCurrent);
+    end
+    major1 = major;
+    minor1 = minor;
+    patch1 = patch + 1;
+    snapshot1 = true;
+    txt1 = textVersion(major1, minor1, patch1, snapshot1);
+    userTxt1 = input(sprintf('Next version [%s]:\n', next), 's');
+    if ~isempty(userTxt1)
+        [major1 minor1 patch1 snapshot1 txt1] = replab_version(userNext);
+    end
+    disp('Preparing release');
+    disp(sprintf('Setting version to %s'), txt);
+    fid = fopen('replab_version.txt', 'w');
+    assert(fid ~= -1);
+    fprintf(fid, '%s\n', txt);
+    status = fclose(fid);
+    assert(status == 0);
+    disp('Committing version change.');
+    status = system('git add replab_version.txt');
+    assert(status == 0);
+    status = system(sprintf('git commit -m "Setting version to %s"', txt));
+    assert(status == 0);
+    status = system('git push origin master', '-echo');
+    assert(status == 0);
+    status = system(sprintf('git tag v%s', txt));
+    assert(status == 0);
+    status = system(sprintf('git push origin v%s', txt), '-echo');
+    assert(status == 0);
+    disp(sprintf('Advancing to next version number %s', txt));
+    fid = fopen('replab_version.txt', 'w');
+    assert(fid ~= -1);
+    fprintf(fid, '%s\n', txt1);
+    status = fclose(fid);
+    assert(status == 0);
+    disp('Committing version change.');
+    status = system('git add replab_version.txt');
+    assert(status == 0);
+    status = system(sprintf('git commit -m "Setting version to %s"', txt1));
+    assert(status == 0);
+    status = system('git push origin master', '-echo');
+    assert(status == 0);
+    function txtV = textVersion(majorV, minorV, patchV, snapshotV)
+        if snapshotV
+            suffixV = '-SNAPSHOT';
+        else
+            suffixV = '';
+        end
+        txtV = sprintf('%d.%d.%d%s', majorV, minorV, patchV, suffixV);
+    end
+end
--- replab_version.m
+++ replab_version.m
@@ -1,10 +1,24 @@
-function [Major Minor Patch] = replab_version
-% Returns RepLAB version read, from "replab_version.txt" 
+function [major minor patch snapshot contents] = replab_version(contents)
+% Parses a RepLAB version number
+%
+% If contents is provided, then parses that string; otherwise reads the version
+% string from the replab_version.txt file.
     [pathStr, name, extension] = fileparts(which(mfilename));
     version_filename = fullfile(pathStr, 'replab_version.txt');
     contents = fileread(version_filename);
-    mmp = strsplit(contents, '.');
-    Major = str2num(mmp{1});
-    Minor = str2num(mmp{2});
-    Patch = str2num(mmp{3});
+    contents = strrep(contents, sprintf('\n'), '');
+    contents = strrep(contents, sprintf('\r'), '');    
+    contents = strtrim(contents);
+    snapshotString = '-SNAPSHOT';
+    if length(contents) > length(snapshotString) && isequal(contents(end-length(snapshotString)+1:end), snapshotString)
+        snapshot = true;
+        mmp = contents(1:end-length(snapshotString));
+    else
+        snapshot = false;
+        mmp = contents;
+    end
+    mmp = strsplit(mmp, '.');
+    major = str2num(mmp{1});
+    minor = str2num(mmp{2});
+    patch = str2num(mmp{3});
 end
