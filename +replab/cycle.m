3f9604640bb3eba5c862193cbfbfe89e6d293e70
Bogdan <34404779+bogdan98@users.noreply.github.com>
2019-01-27 12:12:43 +0000

    Add files via upload
    Construction of Mathieu groups. Obtained orders match with theory.

---
+++ +replab/cycle.m
@@ -0,0 +1,15 @@
+function [input] = cycle(input, varargin)
+
+%convention used - cycle of (123) corresponds to 1 -> 2, 2 -> 3, 3 -> 1
+
+for j = 1:length(varargin)
+    
+    cycle = varargin{j};
+    c = length(cycle);
+    in = input(cycle(1));
+    for i = 1:c-1
+       input(cycle(i)) = input(cycle(i + 1));
+    end
+    input(cycle(c)) = in;
+end
+
