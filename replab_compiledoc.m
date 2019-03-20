a03a92c0ddb0b1eae44e22c7c44e5a7e9d79fa4f
Jean-Daniel Bancal <jdbancal@gmail.com>
2019-03-20 14:25:28 +0100

    Title appears on the page

--- replab_compiledoc.m
+++ replab_compiledoc.m
@@ -50,13 +50,14 @@ function replab_compiledoc
     command = @(filename) publish(filename, 'stylesheet', '../stylesheet/mxdom2jekyll.xsl');
     cellfun(command, scripts, 'UniformOutput', 0);
 
-    % We remove the matlab trademark and copy files over
+    % We copy the files over and make the necessary adjustments
     copyfile('html/*','../../docs/docs/publish/');
     co = 0;
     for file = scripts
         co = co + 1;
         fidIn = fopen(['html/', file{1}(1:length(file{1})-2), '.html']);
         fidOut = fopen(['../../docs/docs/publish/', file{1}(1:length(file{1})-2), '.html'], 'w');
+        title = '';
         while 1
             tline = fgets(fidIn);
             if ~ischar(tline)
@@ -66,6 +67,18 @@ function replab_compiledoc
             % If needed, we add the menu number
             if (menuPositions(co) ~= 0) && (length(tline) == 15) && isequal(tline(1:14), 'comments: true')
                 fprintf(fidOut, '%s\n', ['position: ', num2str(menuPositions(co))]);
+            end
+            
+            % add the page title
+            % if we find a page title, we remember it
+            if (length(tline) >= 7) && isequal(tline(1:7), 'title: ')
+                title = erase(tline(8:end), char(10));
+            end
+            % if we find the beginning of the introduction, we add the title to it
+            found = strfind(tline, '<!--introduction-->');
+            if ~isempty(found) && ~isempty(title)
+                found = found(1);
+                tline = [tline(1:found+18), '<h1>', title, '</h1>', tline(found+19:end)];
             end
             
             % We remove any occurence of string like that:
