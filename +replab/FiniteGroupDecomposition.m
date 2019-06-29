bfdb6e80cc859636df8063d11b6e041a88d3bd90
Denis Rosset <physics@denisrosset.com>
2019-06-29 15:53:29 -0400

    Group decomposition has identity as first element.

--- +replab/FiniteGroupDecomposition.m
+++ +replab/FiniteGroupDecomposition.m
@@ -4,12 +4,14 @@ classdef FiniteGroupDecomposition < replab.Str
 % Describes the set U = { u }, where u = u_1 u_2 ... u_n and
 % u_i is in transversals{i}
 %
+% For each i, we have transversals{i}{1} the group identity.
+%
 % The set U possibly has repetitions of elements, and composition
 % of elements is done according to the binary operation of "group".
 %
 % A construction comes from the decomposition of a group into
 % a chain of subgroups
-% G = G_1 >= G_2 >= ... G_{n+1} = <>
+% G = G_1 >= G_2 >= ... G_{n+1} = trivial group
 % where transversals{i} are (left) coset representatives of G_i / G_{i+1}
     properties (SetAccess = protected)
         group;
