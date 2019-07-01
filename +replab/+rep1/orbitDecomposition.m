117f5adba6199549aa6eef97949860500fd60931
Denis Rosset <physics@denisrosset.com>
2019-07-01 12:03:25 -0400

    Orbit decomposition of representations.

--- +replab/+rep1/orbitDecomposition.m
+++ +replab/+rep1/orbitDecomposition.m
@@ -1,4 +1,4 @@
-function [orbits bases] = orbitDecomposition(rep)
+function [sub orbits] = orbitDecomposition(rep)
 % Identifies subrepresentations from the sparsity pattern of a representation
 %
 % Used as a first step in the decomposition into irreducibles
@@ -10,10 +10,11 @@ function [orbits bases] = orbitDecomposition(rep)
     end
     orbits = replab.Partition.connectedComponents(mask);
     n = orbits.nBlocks;
-    bases = cell(1, n);
+    sub = cell(1, n);
     for b = 1:n
         block = orbits.block(b);
         d = length(block);
-        bases{b} = sparse(block, 1:d, ones(1, d), d, rep.dimension);
+        basis = sparse(block, 1:d, ones(1, d), rep.dimension, d);
+        sub{b} = rep.subRep(basis);
     end
 end
