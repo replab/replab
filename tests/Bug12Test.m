81b1fd8ab4cf665d3dc7d3569c5aa85f66c7d018
Denis Rosset <physics@denisrosset.com>
2019-07-09 22:12:07 -0400

    Closes #12.

---
+++ tests/Bug12Test.m
@@ -0,0 +1,10 @@
+function test_suite = Bug12Test()
+    try
+        test_functions = localfunctions();
+    catch
+    end
+    initTestSuite;
+    group = replab.S(9).subgroup({[7 4 1 9 5 2 6 8 3] [7 3 4 2 5 6 9 8 1]});
+    I = group.naturalRep.decomposition;
+    test_suite = replab.IrreducibleLaws(I).addTestCases(test_suite);
+end
