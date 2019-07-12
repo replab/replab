f51f07f649f93d78c0de2e72d21f28fd61419e12
Denis Rosset <physics@denisrosset.com>
2019-07-12 10:59:50 +0100

    Octave compatibility

--- tests/Bug12Test.m
+++ tests/Bug12Test.m
@@ -4,7 +4,8 @@ function test_suite = Bug12Test()
     catch
     end
     initTestSuite;
-    group = replab.S(9).subgroup({[7 4 1 9 5 2 6 8 3] [7 3 4 2 5 6 9 8 1]});
+    S9 = replab.S(9);
+    group = S9.subgroup({[7 4 1 9 5 2 6 8 3] [7 3 4 2 5 6 9 8 1]});
     I = group.naturalRep.decomposition;
     test_suite = replab.IrreducibleLaws(I).addTestCases(test_suite);
 end
