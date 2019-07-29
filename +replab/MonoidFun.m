f125da1bc9a930d9b9d3958b83b99c648ff71bc8
Denis Rosset <physics@denisrosset.com>
2019-07-29 14:44:29 -0400

    Merged Semigroup into Monoid (#49)

--- +replab/MonoidFun.m
+++ +replab/MonoidFun.m
@@ -1,16 +1,23 @@
-classdef MonoidFun < replab.SemigroupFun & replab.Monoid
+classdef MonoidFun < replab.DomainFun & replab.Monoid
+    
+    properties (SetAccess = protected)
+        composeFun;
+    end
+
     methods
+        
         function self = MonoidFun(description, eqvFun, sampleFun, ... % Domain
-                                     composeFun, ... % Semigroup
-                                     identity) % Monoid
-            self@replab.SemigroupFun(description, eqvFun, sampleFun, composeFun);
+                                     composeFun, identity) % Monoid
+            self@replab.DomainFun(description, eqvFun, sampleFun);
+            self.composeFun = composeFun;
             self.identity = identity;
         end
+        
         function names = hiddenFields(self)
             names = replab.str.uniqueNames( ...
-                hiddenFields@replab.SemigroupFun(self), ...
+                hiddenFields@replab.DomainFun(self), ...
                 hiddenFields@replab.Monoid(self), ...
-                {'inverseFun'} ...
+                {'composeFun'} ...
                 );
         end
     end
