4171611e5ea67afc2c29a2839d2cbc33ebd86218
Denis Rosset <physics@denisrosset.com>
2019-08-19 16:30:42 -0400

    Making replab.Commutant an abstract base class.

--- +replab/+rep/commutant.m
+++ +replab/+rep/commutant.m
@@ -2,6 +2,6 @@ function c = commutant(rep)
 % Constructs the algebra of matrices that commute with rep
 %
 % See replab.Commutant for a more complete description
-    c = replab.Commutant(rep);
+    c = replab.rep.DecompositionCommutant(rep);
     % placeholder function for future optimizations
 end
