badd33d356620a829e4f30ebdd2b8b06217919a2
Jean-Daniel Bancal <jdbancal@gmail.com>
2019-09-16 11:10:04 +0200

    bug tests are probably not needed for coverage

--- tests/Bug12Test.m
+++ tests/Bug12Test.m
@@ -4,6 +4,11 @@ function test_suite = Bug12Test()
     catch
     end
     initTestSuite;
+    
+    if ReplabTestParameters.onlyFastTests
+        return;
+    end
+    
     S9 = replab.S(9);
     group = S9.subgroup({[7 4 1 9 5 2 6 8 3] [7 3 4 2 5 6 9 8 1]});
     I = group.naturalRep.decomposition;
