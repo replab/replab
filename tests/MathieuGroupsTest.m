3c23a450aaeb6846d3406951ae57546942a6ab2e
Denis Rosset <physics@denisrosset.com>
2019-09-18 16:37:48 -0400

    Added group order tests by @bogdan98

---
+++ tests/MathieuGroupsTest.m
@@ -0,0 +1,48 @@
+function test_suite = MathieuGroupsTest()
+    disp(['Setting up tests in ', mfilename()]);
+    try
+        test_functions = localfunctions();
+    catch
+    end
+    initTestSuite;
+end
+
+function test_M11
+% Mathieu group M11
+    g1 = cycle([1:11], [2,10], [4,11], [5,7], [8,9]);
+    g2 = cycle([1:11], [1,4,3,8], [2,5,6,9]);
+    M11 = replab.Permutations(11).subgroup({g1, g2});
+    assert(M11.order == 7920);
+end
+
+function test_M12
+% Mathieu group M12
+    g1 = cycle([1:12], [1,4], [3,10], [5,11], [6,12]);
+    g2 = cycle([1:12], [1,8,9], [2,3,4], [5,12,11], [6,10, 7]);
+    M12 = replab.Permutations(12).subgroup({g1, g2});
+    assert(M12.order == 95040);
+end
+
+function test_M22
+% Mathieu group M22
+    g1 = cycle([1:22], [1,13], [2,8], [3,16], [4,12], [6,22], [7,17], [9, 10], [11,14]);
+    g2 = cycle([1:22], [1,22,3,21], [2,18,4,13], [5,12], [6,11,7,15], [8,14,20,10], [17, 19]);
+    M22 = replab.Permutations(22).subgroup({g1, g2});
+    assert(M22.order == 443520);
+end
+
+
+function test_M23
+% Mathieu group M23
+    g1 = cycle([1:23], [1,2], [3,4], [7,8], [9,10], [13,14], [15,16], [19,20], [21,22]);
+    g2 = cycle([1:23], [1,16,11,3], [2,9,21,12], [4,5,8,23], [6,22,14,18], [13,20], [15,17]);
+    M23 = replab.Permutations(23).subgroup({g1, g2});
+    assert(M23.order == 10200960);
+end
+
+function test_M24
+    g1 = cycle([1:24], [1,4], [2,7], [3,17], [5,13], [6,9], [8,15], [10, 19], [11, 18], [12, 21], [14, 16], [20, 24], [22, 23]);
+    g2 = cycle([1:24], [1,4,6], [2,21,14], [3, 9, 15], [5, 18, 10], [13, 17, 16], [19, 24, 23]);
+    M24 = replab.Permutations(24).subgroup({g1, g2});
+    assert(M24.order == 244823040);
+end
