cd3c3f132ce1d12e0df7d11b0a61ecf04349c0cb
Denis Rosset <physics@denisrosset.com>
2019-09-17 22:44:56 -0400

    Documentation of replab.rep.* stuff

--- src/+replab/+rep/vecgcd.m
+++ src/+replab/+rep/vecgcd.m
@@ -1,4 +1,11 @@
 function g = vecgcd(vec)
+% Returns the greatest common divisor of a vector of integers
+%
+% Args:
+%   vec (vector of integer): Integer to compute the GCD of
+%
+% Returns:
+%   integer: The computed GCD
     assert(length(vec) > 0);
     g = vec(1);
     for i = 2:length(vec)
