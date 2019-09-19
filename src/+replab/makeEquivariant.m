8af6d913245c616fcf3f87114ec8c41de5fbf52f
Denis Rosset <physics@denisrosset.com>
2019-09-19 14:30:34 -0400

    Documentation

--- src/+replab/makeEquivariant.m
+++ src/+replab/makeEquivariant.m
@@ -1,3 +1,17 @@
 function E = makeEquivariant(repR, repC)
+% Returns the space of equivariant linear maps between two representations
+%
+% The equivariant vector space contains the matrices X such that
+%
+% self.image(g) * X = X * repC.image(g)
+%
+% Note: default implementations are registered in `replab.dispatchDefaults`
+%
+% Args:
+%   repR (replab.Rep): Representation on the target/row space
+%   repC (replab.Rep): Representation on the source/column space
+%
+% Returns:
+%   replab.Equivariant: The equivariant vector space
     E = replab.dispatch('call', 'replab.makeEquivariant', repR, repC);
 end
