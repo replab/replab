5e03d1e282968ebb6838c997935ec916d84ef29a
Denis Rosset <physics@denisrosset.com>
2019-10-15 18:29:27 +0200

    Test correct construction of wreath product subgroups

--- tests/WreathProductGroupTest.m
+++ tests/WreathProductGroupTest.m
@@ -10,3 +10,10 @@ function test_suite = WreathProductGroupTest()
     W = S3.wreathProduct(S3);
     test_suite = replab.FiniteGroupLaws(W).addTestCases(test_suite);
 end
+
+function test_wreath_subgroup
+    S2 = replab.Permutations(2);
+    W = S2.wreathProduct(S2);
+    g = {[2 1] {[1 2] [2 1]}};
+    assertEqual(W.subgroup({g}).order, 4);
+end
