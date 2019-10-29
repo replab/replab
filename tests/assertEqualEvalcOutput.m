bc009562e7408518ed32d4dbc9c0626f70353b86
Denis Rosset <physics@denisrosset.com>
2019-10-29 17:45:16 -0400

    Filter noise in evalc output

--- tests/assertEqualEvalcOutput.m
+++ tests/assertEqualEvalcOutput.m
@@ -5,13 +5,21 @@ function assertEqualEvalcOutput(obtainedString, expectedLines, message)
 %   obtainedString (char): Output string of `evalc`
 %   expectedLines (cell array of char): Expected lines as strings in a cell row vector
 %   message (char) : message to be printed in case of failure
-    obtainedLines = strsplit(obtainedString, '\n');
-    filteredLines = {};
-    for i = 1:length(obtainedLines)
-        l = strtrim(obtainedLines{i});
-        if length(l) > 0
-            filteredLines{1, end+1} = l;
+    obtainedLines = filter(strsplit(obtainedString, '\n'));
+    expectedLines = filter(expectedLines);
+    assertEqual(obtainedLines, expectedLines, message);
+end
+function filtered = filter(lines)
+    filtered = {};
+    for i = 1:length(lines)
+        l = lines{i};
+        l = regexprep(l, '<a +href=".*?>', '');
+        l = regexprep(l, '</a>', '');
+        l = regexprep(l, '.\x08', '');
+        l = strrep(l, 'ans =', '');
+        l = strtrim(l);
+        if length(l) > 0 && ~isequal(l, 'logical')
+            filtered{1, end+1} = l;
         end
     end
-    assertEqual(filteredLines, expectedLines, message);
 end
