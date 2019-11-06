af49c42c9b53ae09d25ad2a8459d82274dc792fa
Denis Rosset <physics@denisrosset.com>
2019-11-06 16:56:22 -0500

    Progress on Sphinx documentation (#129)

---
+++ sphinxdocs/base.rst
@@ -0,0 +1,25 @@
+Base object types in RepLAB
+===========================
+
+The following classes are at the base of many RepLAB objects.
+  
+.. module:: +replab
+
+* `.Str`: provides infrastructure for pretty printing of objects, in a way compatible with Matlab and Octave
+* `.Domain`: describes a set of elements that can be compared for equality, and can also be sampled from
+
+.. _Str:
+Str
++++
+
+.. autoclass:: Str
+   :show-inheritance:
+   :members:
+      
+.. _Domain:
+Domain
+++++++
+      
+.. autoclass:: Domain
+   :show-inheritance:
+   :members:
--- src/+replab/Str.m
+++ src/+replab/Str.m
@@ -7,7 +7,17 @@ classdef Str < handle
     methods
         
         function res = eq(self, rhs)
+        % eq - equality test
+        %
+        % res = replab.Str.eq(self, rhs)
         % Workaround bug of == not implemented for handles
+        %
+        % Args:
+        %   self (object): first object
+        %   rhs (object): second object to compare to
+        %
+        % Returns:
+        %   res (boolean): true iff self == rhs
             if replab.platformIsOctave
                 res = true(size(self));
             else
@@ -23,36 +33,51 @@ classdef Str < handle
             disp(strjoin(lines, '\n'));
         end
         
-        function [names values] = additionalFields(self)
-        % Returns the name/value pairs corresponding to additional
-        % fields to be printed; given as row vectors
-        % Classes that override this method should call the
-        % superclass method
+        function [names, values] = additionalFields(self)
+        % additionalFields - additional name/value pairs
+        %
+        % [names, values] = replab.Str.additionalFields(self)
+        % Returns the name/value pairs corresponding to additional fields
+        % to be printed. Classes that override this method should call the
+        % superclass method.
+        %
+        % Returns:
+        %   names (cell array): row vector of names
+        %   values (cell array): row vector of values
             names = {};
             values = {};
         end
         
         function names = hiddenFields(self)
+        % hiddenFields - Fields that should not be printed
+        %
+        % names = replab.Str.hiddenFields(self)
         % Returns the names of the fields that are not printed as a row vector
         % Classes that override this method should call the superclass method
+        %
+        % Returns:
+        %   names (cell array): row vector of names
             names = {};
         end
         
         function s = shortStr(self, maxColumns)
-        % Returns a single line description of the current object
-        % see replab.shortStr for documentation
+        % shortStr - Single line text description of the current object
+        %
+        % See also: replab.shortStr
             s = replab.str.shortStr(self, maxColumns);
         end
         
         function s = headerStr(self)
-        % Returns a tiny single line description of the current object type
-        % see replab.headerStr for documentation
+        % headerStr - Tiny single line description of the current object type
+        %
+        % See also replab.headerStr
             s = replab.str.headerStr(self);
         end
         
         function s = longStr(self, maxRows, maxColumns)
-        % Returns a multi line description of the current object
-        % see replab.longStr for documentation
+        % longStr - Multi-line description of the current object
+        %
+        % See also: replab.longStr
             s = replab.str.longStr(self, maxRows, maxColumns);
         end
 
