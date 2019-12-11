e1d18c7b0c7f8527735a623472ade970848dc23a
Unknown <jdbancal@gmail.com>
2019-12-10 21:01:17 -0500

    octave compatibility

--- src/+replab/+infra/Function.m
+++ src/+replab/+infra/Function.m
@@ -30,7 +30,7 @@ classdef Function < replab.Str
         %
         % Args:
         %   declaration (charstring): Trimmed function/method declaration line
-            if contains(declaration, '=')
+            if sum(declaration == '=') >= 1
                 parts = strsplit(declaration, '=');
                 assert(length(parts) == 2);
                 tokens = regexp(strtrim(parts{2}), '^(\w+)', 'tokens', 'once');
