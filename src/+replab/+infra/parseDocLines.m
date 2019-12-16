62efe60c944c89b56828248806291e9535de1dcd
Denis Rosset <physics@denisrosset.com>
2019-12-16 18:43:05 -0500

    Parse doc lines includes parsing the doc line numbers

--- src/+replab/+infra/parseDocLines.m
+++ src/+replab/+infra/parseDocLines.m
@@ -1,12 +1,14 @@
-function [pos docLines] = parseDocLines(ct, pos)
+function [pos docLines docLineNumbers] = parseDocLines(ct, pos)
 % Parses zero, one or more lines of comments and returns them with the leading % stripped
     docLines = {};
+    docLineNumbers = [];
     while 1
         [res line] = ct.expect(pos, '%');
         if isempty(res)
             break
         else
             docLines{1, end+1} = line(2:end);
+            docLineNumbers(1, end+1) = pos;
             pos = res;
         end
     end
