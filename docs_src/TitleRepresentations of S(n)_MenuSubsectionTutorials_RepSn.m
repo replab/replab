40053443c1c7926ca6f3351f8978d237922c17ad
Jean-Daniel Bancal <jdbancal@gmail.com>
2019-12-18 11:50:15 -0500

    WIP

--- docs_src/TitleRepresentations of S(n)_MenuSubsectionTutorials_RepSn.m
+++ docs_src/TitleRepresentations of S(n)_MenuSubsectionTutorials_RepSn.m
@@ -1,6 +1,6 @@
 %% Representations of the symmetric group S(n)
 %
-% The symmetric group $S(n)$ is group of primary importance. This
+% The symmetric group S(n) is group of primary importance. This
 % document presents several representations of this group with **RepLAB**.
 
 %%
@@ -42,38 +42,49 @@ element1 = [2 1 3];
 element2 = [1 3 2];
 S3.compose(element1, element2)
 
-%% Group representations
+%% The defining representation of S(n)
 % A group can always be represented by matrices of finite dimension.
 % In a $d$-dimensional matrix representation, each group element is
-% associated with a d x d matrix called the image of the group element. The
-% matrix multiplication rule then reflects the group composition rule.
+% associated with a $d \times d$ matrix called the image of the group
+% element. The matrix multiplication rule then reflects the group
+% composition rule.
 %
 % Several matrix representations are possible for the same group, possibly
 % with various dimension $d$. Here, we are going to explore some
 % representations of our group S(3).
-
-%% Defining representation of S(n)
 %
-% When permuting n elements, a natural choice of representation consists in
-% associating one dimension to each element, hence choosing $d=n$. Matrices
-% then act on $R^n$. This is called the *defining represention* of
-% a group. It can be constructed easily:
+% When seeing the group as a permutation within $n$ elements, a natural
+% choice of representation arises in dimension $d=n$, where the permutation
+% is applied to the coordinates of the vector space $R^n$. This is called
+% the *defining representation* of a group. It can be constructed easily:
 defRep = S3.definingRep
 %%
 % The image of a group element in this representation can be extracted
 defRep.image([2 1 3])
 %%
-% We see that the matrices here are permutation matrices which simply apply
-% the permutation to the coordinates.
+% We see that the matrices representing the group elements are $n \times n$
+% permutation matrices. which simply apply the permutation to the
+% coordinates of the vector space $R^n$.
 %
-% Let us check that the representation is faithful, i.e. that is follows
+% Let us check that the representation is valid, i.e. that is follows
 % indeed the group structure
 image1 = defRep.image(element1);
 image2 = defRep.image(element2);
 image1*image2
 defRep.image(S3.compose(element1, element2))
 %%
-% The two images are indeed 
+% We see that the image of the product of elements is indeed the product of
+% the images of the respective elements, i.e. the algebra of matrices
+% acting on the representation images reflects accurately the group
+% algebra.
+%
+% It can be checked, moreover, that the defining representation is
+% faithful, i.e. each group element has its own distinct image. This needs
+% not always be the case as in the following two examples.
+
+%% The trivial representation of S(n)
+
+% Whether computed from, or from, we see that the matrix representing the permutation [2 3 1] ise
 
 %
 % The action of a group can always be represented by the product of d x d
@@ -86,6 +97,9 @@ defRep.image(S3.compose(element1, element2))
 % The action of a group can be represented by the matrix product acting o.
 
 
+%% Faithful representation
+
+%% Trivial representation
 
 
 
