f1932cf8d24ad63cf7826c06c2b4d92f369096a1
Denis Rosset <physics@denisrosset.com>
2020-01-04 23:11:37 -0500

    Retrieval of all tests in a class/function.

--- src/+replab/+infra/+doctests/namedTestsInElement.m
+++ src/+replab/+infra/+doctests/namedTestsInElement.m
@@ -1,15 +1,33 @@
-function s = namedTestsInElement(el)
+function s = namedTestsInElement(sel)
 % Extracts all tests in a source element and its potential members
 %
 % When called on a class, it includes the tests of the class members.
 %
 % Args:
-%   el (`replab.infra.SourceElement`): Source element to extract tests from
+%   sel (`replab.infra.SourceElement`): Source element to extract tests from
 %
 % Returns:
 %   struct: A structure whose field names are the test names, and values
 %           are row cell vectors of `.DocTest`, with their line numbers
 %           referring to the position in the original filename.
     s = struct;
-    doctests = replab.infra.DocTest.
+    dt = replab.infra.doctests.testsInElement(sel);
+    if ~isempty(dt)
+        s.(sel.name) = dt;
+    end
+    if isa(sel, 'replab.infra.Class')
+        cels = struct2cell(sel.ownElements);
+        for i = 1:length(cels)
+            cel = cels{i};
+            dt = replab.infra.doctests.testsInElement(cel);
+            if ~isempty(dt)
+                if isfield(s, cel.name)
+                    newValue = horzcat(s.(cel.name), dt);
+                else
+                    newValue = dt;
+                end
+                s.(cel.name) = newValue;
+            end
+        end
+    end
 end
