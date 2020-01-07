e0b37a68009e3c33dabc9d5759a9d3d282f4218e
Jean-Daniel Bancal <jdbancal@gmail.com>
2020-01-07 11:04:21 +0100

    standard c++ doesn't support run-time array dimensions

--- src/+replab/+graph/burningAlgorithmFast_mex.cpp
+++ src/+replab/+graph/burningAlgorithmFast_mex.cpp
@@ -6,16 +6,16 @@
 #include <functional>
 
 /*
-  This files is part of the RepLAB library. It provides a faster 
+  This files is part of the RepLAB library. It provides a faster
   implementation of the algorithm contained in burning.m.
-  
-  For optimal efficiency, the edges are not checked for redundency. 
-  Hence, it is best to provide each edge only once (in particular, the 
+
+  For optimal efficiency, the edges are not checked for redundency.
+  Hence, it is best to provide each edge only once (in particular, the
   edge "1-2" already implies "2-1" so the latter should not be given).
-  If we wish to change the code to eliminate redundancy, the data 
+  If we wish to change the code to eliminate redundancy, the data
   structure should be changed from vector < vector < long int > > to
-  vector < set < long int > >.  
-  
+  vector < set < long int > >.
+
   This implementation supports sparse inputs, such as [1 2; 1 n] with n
   very large; with an overhead in memory and time much smaller than n.
 */
@@ -36,7 +36,7 @@ struct compareFirstPart {
 };
 
 
-/* This is the function that is called from matlab. It has just one 
+/* This is the function that is called from matlab. It has just one
    possible calling pattern:
     - prhs should point to a matrix of size n x 2 of undirected edges
     - nlhs will be returned as a cell array of groups of connex vertices
@@ -47,7 +47,7 @@ struct compareFirstPart {
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
   //-//-// Argument checking //-//-//
-  
+
   // We check that the parameters are correct
   if (nrhs != 1)
     mexErrMsgTxt("burningAlgorithm_mex: Unexpected number of arguments.");
@@ -57,7 +57,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   // The matlab object is supposed to be an array
   if (!mxIsDouble(prhs[0]))
     mexErrMsgTxt("burningAlgorithm_mex: The argument should be an array of double.");
-  
+
   // Get the size and pointers to input data
   mwSize m(mxGetM(prhs[0]));
   mwSize n(mxGetN(prhs[0]));
@@ -72,11 +72,11 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   // Second dimension should be 2
   if (n != 2)
     mexErrMsgTxt("burningAlgorithm_mex: The input should be of dimension m x 2.");
-  
-  
-  
+
+
+
   //-//-// Data initialization //-//-//
-  
+
   // First, we quickly list the vertices numbers in a compact vector
   set < Index > uniqueVertices(pr, pr + m*n); // This efficiently removes duplicates
   vector < Index > vertices(uniqueVertices.begin(), uniqueVertices.end()); // We extract the unique vertices into a vector
@@ -93,13 +93,13 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   {
     cout << i << " == " << verticesInverse(vertices[i]) << endl;
   }*/
-  
+
   // We initialize the graph data structure
   vector < vector < Index > > graphData(nbVertices);
   for (mwIndex i = 0; i < m; ++i) {
     Index a(*(pr+i));
     Index b(*(pr+i+m));
-    
+
     // We map these numbers to the new compact indices
     Index newA(verticesInverse(a));
     Index newB(verticesInverse(b));
@@ -112,11 +112,9 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
 
   //-//-// Algorithm //-//-//
-  
-  // Now we perform the actual burning algorithm
-  Index reached [nbVertices];
-  fill_n(reached, nbVertices, 0);
 
+  // Now we perform the actual burning algorithm
+  vector < Index > reached(nbVertices, 0);
   vector < Index > neighbors [2];
   short int ptr(0);
   neighbors[ptr].push_back(0); // We begin the algorithm by reaching the first site 0.
@@ -141,7 +139,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
       }
       ptr = 1-ptr;
     } while (neighbors[ptr].size() > 0);
-    
+
     // We look for the next un-attained vertex
     for (Index i(0); i < nbVertices; ++i) {
       if (reached[i] == 0) {
@@ -150,7 +148,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
       }
     }
   } while (neighbors[ptr].size() > 0);
-  
+
 
 
   //-//-// Preparing output fields //-//-//
@@ -162,6 +160,6 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     copy(allSets[i].begin(), allSets[i].end(), mxGetPr(oneFamily)); // copy the data
     mxSetCell(plhs[0], i, oneFamily); // Assign the data to the cell element
   }
-  
+
   return;
 }
