abe996726edcf2f78deb24cba52d58fa3ee564d0
Denis Rosset <physics@denisrosset.com>
2020-01-08 17:18:38 -0500

    Uncollapse annotations on anchor click.

--- sphinxdocs/_static/js/collapse_helper.js
+++ sphinxdocs/_static/js/collapse_helper.js
@@ -10,4 +10,20 @@ $(document).ready(function() {
 	    }
 	}
     }
+    $('a').click(function(){
+	target = $( this ).attr('href');
+	pos = target.indexOf('#');
+	if (pos != -1) {
+	    hash = target.substr(pos);
+            base = hash.replace(/\./g, '\\.');
+            base = base.replace(/\+/g, '\\+');
+            admonitions = $('.admonition:has(' + base + ')');
+	    for (i = 0; i < admonitions.length; i ++) {
+		admonition = admonitions[i];
+		if (admonition.classList.contains("collapsed")) {
+		    admonition.classList.remove("collapsed");
+		}
+	    }
+	}
+    });
 });
