cca46f1dea31e7d315da379f2f5393ea426acdb1
Denis Rosset <physics@denisrosset.com>
2020-01-21 02:16:52 -0500

    Corrected laws examples for Octave

--- src/+replab/Str.m
+++ src/+replab/Str.m
@@ -21,18 +21,16 @@ classdef Str < handle
 %           parent: Permutations acting on 3 elements
 %     generator(1): [2, 3, 1]
 %     generator(2): [2, 1, 3]
-%   >>> replab.longStr(P)
+%   >>> strjoin(replab.longStr(P), '\n')
 %   ans =
-%    6x1 cell array
-%     {'Permutations acting on 3 elements'              }
-%     {'  domainSize: 3                                '}
-%     {'    identity: [1, 2, 3]                        '}
-%     {'      parent: Permutations acting on 3 elements'}
-%     {'generator(1): [2, 3, 1]                        '}
-%     {'generator(2): [2, 1, 3]                        '}
+%     Permutations acting on 3 elements'
+%       domainSize: 3
+%         identity: [1, 2, 3]
+%           parent: Permutations acting on 3 elements
+%     generator(1): [2, 3, 1]
+%     generator(2): [2, 1, 3]
 %   >>> replab.shortStr(P)
-%     ans =
-%       'Permutations acting on 3 elements'
+%     ans = Permutations acting on 3 elements
     methods
 
         function res = eq(self, rhs)
