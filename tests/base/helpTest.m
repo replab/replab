32b6f0e78161dd54180a2d122bac19c13695ddeb
Jean-Daniel Bancal <jdbancal@gmail.com>
2020-02-19 11:34:22 +0100

    basic test coverage for help

---
+++ tests/base/helpTest.m
@@ -0,0 +1,33 @@
+function test_suite = helpTest()
+    disp(['Setting up tests in ', mfilename()]);
+    try
+        test_functions = localfunctions();
+    catch
+    end
+    initTestSuite;
+    
+    % Note: here we mostly generate some code coverage
+end
+
+function test_help_external
+    replab_init
+    assert(~isempty(evalc('help plot')));
+end
+
+function test_help_package
+    assert(~isempty(evalc('help replab')));
+end
+
+function test_help_class
+    assert(~isempty(evalc('help replab.Monoid')));
+    assert(~isempty(evalc('help -f replab.Monoid')));
+    assert(~isempty(evalc('help replab.Monoid.identity')));
+    assert(~isempty(evalc('help -f replab.Monoid.identity')));
+    assert(~isempty(evalc('help replab.Monoid.compose')));
+    assert(~isempty(evalc('help -f replab.Monoid.compose')));
+end
+
+function test_help_function
+    assert(~isempty(evalc('help replab.S')));
+    assert(~isempty(evalc('help -f replab.S')));
+end
