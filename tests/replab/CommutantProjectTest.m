99157be804cd67327294ca7ce9f2039fd5e0dffa
Denis Rosset <physics@denisrosset.com>
2020-03-06 23:21:12 -0500

    More tests in project on the commutant.

--- tests/replab/CommutantProjectTest.m
+++ tests/replab/CommutantProjectTest.m
@@ -10,12 +10,16 @@ end
 function test_quaternion_representations
     S3 = replab.S(3);
     W = S3.wreathProduct(replab.signed.Permutations.quaternionGroup);
-    rep = W.primitiveRepFun(@(x) x.naturalRep);
-    X = randn(rep.dimension, rep.dimension);
-    I = rep.decomposition;
-    X1 = I.asSimilarRep.commutant.project(X);
-    X2 = I.commutant.project(X1);
+    rep1 = W.primitiveRepFun(@(x) x.naturalRep);
+    rep2 = rep1.decomposition;
+    rep3 = rep2.asSimilarRep;
+    X = randn(rep1.dimension, rep1.dimension);
+    X1 = rep2.E_internal*rep1.commutant.project(rep2.B_internal*X*rep2.E_internal)*rep2.B_internal;
+    X2 = rep2.commutant.project(X);
+    X3 = rep3.commutant.project(X);
     assert(norm(X1 - X2) < replab.Parameters.doubleEigTol);
+    assert(norm(X1 - X3) < replab.Parameters.doubleEigTol);
+    assert(norm(X2 - X3) < replab.Parameters.doubleEigTol);
 end
 
 function test_complex_representations
