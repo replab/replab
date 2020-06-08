e6bba9094d004d0ff942b9d460c6ce28a56fb1f2
Joscelyn <joscelyn@vdveen.ca>
2020-06-08 16:04:21 -0400

    fixed MaxColumn in elmtStr and conjStr

--- elmtStr.m
+++ elmtStr.m
@@ -2,16 +2,15 @@ function elmtStr(elements)
 % prints out up to 10 of a cell array of elements and the array length
 
 nelmts = 12;
-maxCol = 100;
 
 if length(elements) <= nelmts
     for i = 1:length(elements)
-        disp(replab.str.shortStr(elements{i}, maxCol))
+        disp(replab.str.shortStr(elements{i}, []))
     end
 
 else
     for i = 1:nelmts
-        disp(replab.str.shortStr(elements{i}, maxCol))
+        disp(replab.str.shortStr(elements{i}, []))
     end
     disp(['.. ', num2str(length(elements) - nelmts), ' elements omitted'])
 end
