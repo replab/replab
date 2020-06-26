965891726274f43fe546d3735d55a716952cb508
Andrew <16maib@queensu.ca>
2020-06-25 23:52:22 -0400

    Irrep of Symmetric Group
    Implements function and helper function for irreps of symmtric group.
    Also added a function to calculate the dimension of the irrep associated with any given Young Diagram. Works for all partitions of 9 or lower.
    TODO:
    Comment on helper functions
    Add Tests
    Make tests

--- src/+replab/Permutations.m
+++ src/+replab/Permutations.m
@@ -190,7 +190,20 @@ classdef Permutations < replab.PermutationGroup
                 grp = self.subgroup({g1 g2});
             end
         end
-
+        
+        %% Irreducible Representation Method
+        function rep = irrep(self, partition)
+        % Returns the irreducible representation of a 
+        % symmetric group corresponding a Young Diagram
+        %
+        % Args:
+        %   partition (integer(1,:)): The partition corresponding the
+        %   Young Diagram, with elements listed in decreasing order (e.g [3
+        %   3 1] represents the partition of 7 elements: 7 = 3+3+1
+        % Returns:
+        %   The corresponding irreducible representation
+             rep = replab.sym.SymmetricGroupIrrep(self, partition);
+         end
     end
 
     methods (Static)
@@ -316,6 +329,20 @@ classdef Permutations < replab.PermutationGroup
                 end
             end
         end
+        
+        
+        function dim = irrepDimension(partition)
+        % Returns the dimension of the irreducible representation of a 
+        % symmetric group corresponding a Young Diagram
+        %
+        % Args:
+        %   partition (integer(1,:)): The partition corresponding the
+        %   Young Diagram, with elements listed in decreasing order (e.g [5
+        %   3 1] represents the partition of 9 elements 9 = 5+3+1
+        % Returns:
+        %   The dimension of the corresponding irreducible representation.
+             dim = replab.sym.SymmetricGroupIrrep.dimension(partition);
+         end
 
     end
 
