1c6642a6c6a507415cc022f399452f01542dc033
Andrew-Buchanan-boop <65173932+Andrew-Buchanan-boop@users.noreply.github.com>
2020-07-02 18:25:00 -0400

    Irreducible representations of the symmetric group (#264)
    Implements function and helper function for irreps of symmtric group.
    
    Also added a function to calculate the dimension of the irrep associated with any given Young Diagram. 
    
    Works for all partitions of 9 or lower.
    
    Co-authored-by: Andrew <16maib@queensu.ca>
    Co-authored-by: Denis Rosset <physics@denisrosset.com>

--- src/+replab/PermutationGroup.m
+++ src/+replab/PermutationGroup.m
@@ -544,7 +544,7 @@ classdef PermutationGroup < replab.NiceFiniteGroup
         % subgroups of the symmetric group.
         %
         % It corresponds to the representation orthogonal to the
-        % trivial representation with basis [1, 1, ..., 1]'/sqrt(d)
+        % trivial representation with basis ``[1, 1, ..., 1]'/sqrt(d)``
         %
         % Returns:
         %   `+replab.Rep`: The (real) standard representation
@@ -552,6 +552,44 @@ classdef PermutationGroup < replab.NiceFiniteGroup
             rho = self.naturalRep.subRep(B_internal, E_internal);
         end
 
+        function rho = signRep(self)
+        % Returns the sign representation of this permutation
+            rho = replab.RepByImages.fromImageFunction(self, 'R', 1, @(g) replab.PermutationGroup.sign(g));
+        end
+
+    end
+
+    methods(Static)
+
+        function sign = sign(perm)
+        % Returns the sign of a given permutation
+        %
+        % Args:
+        %   perm (permutation): Vector representing a permutation (e.g. [3 2 1 4])
+        %
+        % Returns:
+        %   integer: Sign of the permutation
+            x = perm;
+            n = length(x);
+            oddOrEven = 0; %Records whether the total permutation is odd or even
+            for i = 1:n
+                if x(i) == 0 || x(i) == i %Skip over one cycles and numbers that have been cycled through
+                    continue
+                end
+                cycleSize = -1; %The first element in a cycle isn't counted
+                j = i;
+                while x(j) ~= 0
+                    pHold = x(j);
+                    x(j) = 0;
+                    j = pHold;
+                    cycleSize = cycleSize + 1;
+                end
+                if cycleSize > 0
+                    oddOrEven = oddOrEven + cycleSize; %At the end, this will match the parity (even/odd) of the permuation
+                end
+            end
+            sign = (-1)^mod(round(oddOrEven),2); %Sign of permutation
+            end
     end
 
 end
