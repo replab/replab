16048ae7d0f0d8cadfac3fd43aa00db499f4f090
Denis Rosset <physics@denisrosset.com>
2020-07-04 09:55:36 -0400

    Group intersection test

---
+++ tests/replab/IntersectionTest.m
@@ -0,0 +1,20 @@
+function test_suite = IntersectionTest()
+    disp(['Setting up tests in ', mfilename()]);
+    try
+        test_functions = localfunctions();
+    catch
+    end
+    initTestSuite;
+end
+
+function test_intersection_commutative
+    n = 8;
+    Sn = replab.S(n);
+    f = @() replab.nfg.randomSubgroup(replab.nfg.randomSubgroup(Sn));
+    G1 = f();
+    G2 = f();
+    G3 = f();
+    I1 = G1.intersection(G2.intersection(G3));
+    I2 = G3.intersection(G1.intersection(G2));
+    assert(I1 == I2);
+end
