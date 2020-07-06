77b02816b5ef6d63775f5517c8f949170029eb49
Denis Rosset <physics@denisrosset.com>
2020-07-06 18:37:45 -0400

    Faster computation of minimal points in orbits

--- src/+replab/+bsgs/minimalByBaseOrderingInOrbit.m
+++ src/+replab/+bsgs/minimalByBaseOrderingInOrbit.m
@@ -2,12 +2,38 @@ function mask = minimalByBaseOrderingInOrbit(degree, generators, baseOrdering)
     if size(generators, 2) == 0
         mask = true(1, degree);
     else
-        orbits = replab.Partition.permutationsOrbits(generators').blocks;
+        % simpler code:
+        % orbits = replab.Partition.permutationsOrbits(generators').blocks;
+        % mask1 = false(1, degree);
+        % for i = 1:length(orbits)
+        %     orbit = orbits{i};
+        %     [~, ind] = min(baseOrdering(orbit));
+        %     mask1(orbit(ind)) = true;
+        % end
         mask = false(1, degree);
-        for i = 1:length(orbits)
-            orbit = orbits{i};
-            [~, ind] = min(baseOrdering(orbit));
-            mask(orbit(ind)) = true;
+        done = false(1, degree);
+        i = 1;
+        while ~isempty(i)
+            toCheck = i;
+            minVal = baseOrdering(i);
+            minInd = i;
+            done(i) = true;
+            while 1
+                images = generators(toCheck, :);
+                images = images(:);
+                toCheck = images(~done(images));
+                if isempty(toCheck)
+                    break
+                else
+                    done(toCheck) = true;
+                    [minVal1, minInd1] = min(baseOrdering(toCheck));
+                    if minVal1 < minVal
+                        minInd = toCheck(minInd1);
+                    end
+                end
+            end
+            mask(minInd) = true;
+            i = find(~done, 1);
         end
     end
 end
