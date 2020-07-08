64d27a7b7b3800b5c0fecce4e0a9fd0d88e15ef9
Denis Rosset <physics@denisrosset.com>
2020-07-08 10:53:36 -0400

    WIP

--- src/+replab/identifyGroup.m
+++ src/+replab/identifyGroup.m
@@ -20,26 +20,31 @@ function [name, pres, f, g] = identifyGroup(G)
 %   g:
 %     `+replab.Morphism`: Inverse of the isomorphism ``f``
 
-    [pres x] = replab.id.cyclicGroup(G);
-    if ~isempty(pres)
-        source = replab.PermutationGroup.of(x);
-        n = double(source.order);
-        name = sprintf('Cyclic group of order %d', n);
-        target = replab.CyclicGroup(n);
-        f = source.morphismByImages(target, target.generators);
-        g = target.morphismByImages(source, source.generators);
-        return
-    end
+    if isa(G, 'replab.PermutationGroup')
+        [pres x] = replab.id.cyclicGroup(G);
+        if ~isempty(pres)
+            source = replab.PermutationGroup.of(x);
+            n = double(source.order);
+            name = sprintf('Cyclic group of order %d', n);
+            target = replab.CyclicGroup(n);
+            f = source.morphismByImages(target, target.generators);
+            g = target.morphismByImages(source, source.generators);
+            return
+        end
 
-    [pres x a] = replab.id.dihedralGroup(G);
-    if ~isempty(pres)
-        source = replab.PermutationGroup.of(x, a);
-        d = double(source.order)/2;
-        name = sprintf('Dihedral group of order %d and degree %d', 2*d, d);
-        target = replab.DihedralGroup(d);
-        f = source.morphismByImages(target, target.generators);
-        g = target.morphismByImages(source, source.generators);
-        return
+        [pres x a] = replab.id.dihedralGroup(G);
+        if ~isempty(pres)
+            source = replab.PermutationGroup.of(x, a);
+            d = double(source.order)/2;
+            name = sprintf('Dihedral group of order %d and degree %d', 2*d, d);
+            target = replab.DihedralGroup(d);
+            f = source.morphismByImages(target, target.generators);
+            g = target.morphismByImages(source, source.generators);
+            return
+        end
     end
-
+    name = [];
+    pres = [];
+    f = [];
+    g = [];
 end
