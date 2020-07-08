20f2df56d7e3da98083f1e326c4d4d0f09f946b0
Denis Rosset <physics@denisrosset.com>
2020-07-08 11:00:51 -0400

    Cleaned up Permutation.sign

--- src/+replab/Permutation.m
+++ src/+replab/Permutation.m
@@ -41,19 +41,22 @@ classdef Permutation
         function s = sign(perm)
         % Returns the sign of a given permutation
         %
+        % Example:
+        %   >>> replab.Permutation.sign([3 2 1 4])
+        %       -1
         % Args:
-        %   perm (permutation): Vector representing a permutation (e.g. [3 2 1 4])
+        %   perm (permutation): Permutation to compute the sign of
         %
         % Returns:
         %   integer: Sign of the permutation
             x = perm;
             n = length(x);
-            oddOrEven = 0; %Records whether the total permutation is odd or even
+            s = 1; % Permutation sign, 1 if even, -1 if odd
             for i = 1:n
-                if x(i) == 0 || x(i) == i %Skip over one cycles and numbers that have been cycled through
+                if x(i) == 0 || x(i) == i % Skip over 1-cycles and numbers that have been cycled through
                     continue
                 end
-                cycleSize = -1; %The first element in a cycle isn't counted
+                cycleSize = 0;
                 j = i;
                 while x(j) ~= 0
                     pHold = x(j);
@@ -61,11 +64,10 @@ classdef Permutation
                     j = pHold;
                     cycleSize = cycleSize + 1;
                 end
-                if cycleSize > 0
-                    oddOrEven = oddOrEven + cycleSize; %At the end, this will match the parity (even/odd) of the permuation
+                if mod(cycleSize, 2) == 0 % cycle size is even
+                    s = -s; % flip
                 end
             end
-            s = (-1)^mod(round(oddOrEven),2); %Sign of permutation
         end
 
         function p = sorting(array, greaterFun)
