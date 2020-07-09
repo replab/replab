7a0dba8584c30e1480c1120cb90917cfb46adfaa
Denis Rosset <physics@denisrosset.com>
2020-07-09 15:23:26 -0400

    Documentation of standard group constructions

--- src/+replab/AlternatingGroup.m
+++ src/+replab/AlternatingGroup.m
@@ -1,13 +1,22 @@
 function grp = AlternatingGroup(n)
+% Constructs the alternating group
+%
+% Args:
+%   n (integer): Group degree
+%
+% Returns:
+%   `+replab.PermutationGroup`: The alternating group of degree ``n``
     Sn = replab.S(n);
-    if n <= 2
+    if n <= 2 % special case: group is trivial
         grp = replab.PermutationGroup.trivial(n);
     else
         t = [2 3 1 4:n];
-        if n == 3
+        if n == 3 % special case: it is a cyclic group, one generator only
             grp = Sn.subgroup({t}, Sn.order/2);
             return
         end
+        % generators from page 2100 of
+        % https://www.ams.org/journals/tran/2003-355-05/S0002-9947-03-03040-X/S0002-9947-03-03040-X.pdf
         if mod(n, 2) == 0
             s = [2 1 4:n 3];
         else
