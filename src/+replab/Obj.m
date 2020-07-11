c1f1fd65f2b26d8e14c0bbfaddb396f7b60ad1cc
Denis Rosset <physics@denisrosset.com>
2020-07-11 15:09:57 -0400

    Infrastructure for cached properties.

--- src/+replab/Obj.m
+++ src/+replab/Obj.m
@@ -15,11 +15,106 @@ classdef Obj < replab.Str
     end
 
     properties (Access = protected)
+        cache_ % (struct): Contains the computed properties of this object
         id_ % (uint64): Unique object ID
     end
 
     methods
 
+        function l = inCache(self, name)
+        % Returns whether the value of the given property has already been computed
+        %
+        % Args:
+        %   name (charstring): Name of the property
+        %
+        % Returns:
+        %   logical: Whether the property value is in the cache
+            if isempty(self.cache_)
+                self.cache_ = struct;
+            end
+            l = isfield(self.cache_, name);
+        end
+
+        function cache(self, name, value, handleExisting)
+        % Sets the value of the designated property in the cache
+        %
+        % If the property value is already in the cache, the behavior depends on the argument ``handleExisting``:
+        %
+        % - ``overwrite``: The existing value is overwritten
+        % - ``ignore``: The existing value is left unchanged
+        % - ``isequal``: The existing value and the given value are compared for equality using ``isequal``
+        % - ``=``: The existing value and the given value are compared for equality using ``==``
+        %
+        % Args:
+        %   name (charstring): Name of the property
+        %   value: Value of the property
+        %   handleExisting ({'overwrite', 'ignore', 'isequal', '=='}, optional): What to do if the value is already known, default ``'ignore'``
+            if isempty(self.cache_)
+                self.cache_ = struct;
+            end
+            if self.inCache(name)
+                if nargin <4
+                    handleExisting = 'ignore';
+                end
+                switch handleExisting
+                  case 'overwrite'
+                    self.cache_.(name) = value;
+                  case 'ignore'
+                  case '=='
+                    assert(self.cache_.(name) == value);
+                  case 'isequal'
+                    assert(isequal(self.cache_.(name), value));
+                  otherwise
+                    error('Invalid argument handleExisting')
+                end
+            else
+                self.cache_.(name) = value;
+            end
+        end
+
+        function res = cachedOrEmpty(self, name)
+        % Returns the cached property if it exists, or ``[]`` if it is unknown yet
+        %
+        % See `.cached`.
+        % Args:
+        %   name (charstring): Name of the property
+        %
+        % Returns:
+        %   The property value if known, otherwise ``[]``
+            if isempty(self.cache_)
+                self.cache_ = struct;
+            end
+            if isfield(self.cache_, name)
+                res = self.cache_.(name);
+            else
+                res = [];
+            end
+        end
+
+        function res = cached(self, name)
+        % Returns the cached property if it exists, computing it if necessary
+        %
+        % If the property name is ``thing``, then the object must provide a method named
+        % ``computeThing``. That method will be called of the property has not been computed
+        % before. If the property has been computed before, it will return its value from the
+        % cache.
+        %
+        % Args:
+        %   name (charstring): Name of the property
+        %
+        % Returns:
+        %   The property value
+            if isempty(self.cache_)
+                self.cache_ = struct;
+            end
+            if isfield(self.cache_, name)
+                res = self.cache_.(name);
+            else
+                methodName = ['compute' upper(name(1)) name(2:end)];
+                res = self.(methodName);
+                self.cache_.(name) = res;
+            end
+        end
 
         function i = id(self)
             if isempty(self.id_)
