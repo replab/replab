4f7f9570879bd27f553d17f73d0822c978ac1d82
Denis Rosset <physics@denisrosset.com>
2020-07-13 07:48:27 -0400

    Wip

--- src/+replab/FiniteGroupLaws.m
+++ src/+replab/FiniteGroupLaws.m
@@ -62,6 +62,21 @@ classdef FiniteGroupLaws < replab.GroupLaws
             self.assert(o == self.T.order);
         end
 
+        function law_contains_T(self, t)
+        % Checks element membership (trivial case)
+            self.assert(self.T.contains(t));
+        end
+
+        function law_cyclic_subgroup_order_T(self, t)
+            self.thisIsSlow;
+            if self.T.isIdentity(t)
+                sub = self.T.subgroup({});
+            else
+                sub = self.T.subgroup({t});
+            end
+            self.assert(sub.order == self.T.elementOrder(t));
+        end
+
     end
 
 end
