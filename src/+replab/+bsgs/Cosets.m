5a6b580dc347ef1bfef3270e4223153594fbb896
Denis Rosset <physics@denisrosset.com>
2020-07-15 13:21:52 -0400

    Removed unnecessary sort

--- src/+replab/+bsgs/Cosets.m
+++ src/+replab/+bsgs/Cosets.m
@@ -60,7 +60,7 @@ classdef Cosets
                             stackS{l+1} = stackS{l}.stabilizer(bg);
                             gnext = g(group.u(l, b));
                             stackG(:,l+1) = gnext; % compose transversal element
-                            orbit = sort(group.Delta{l+1}); % TODO: unnecessary sort
+                            orbit = group.Delta{l+1};
                             [~, I] = sort(gnext(orbit));
                             stackB{l+1} = orbit(I);
                             stackM{l+1} = replab.bsgs.minimalMaskInOrbit(n, stackS{l+1}.strongGeneratorsForLevel(1));
