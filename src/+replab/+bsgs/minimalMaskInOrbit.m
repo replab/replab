50da2cd6e7ec52510e68bd9145b773cbe3c4d204
Denis Rosset <physics@denisrosset.com>
2020-07-16 18:22:30 -0400

    Debug code in minimalMaskInOrbit

--- src/+replab/+bsgs/minimalMaskInOrbit.m
+++ src/+replab/+bsgs/minimalMaskInOrbit.m
@@ -5,14 +5,6 @@ function mask = minimalMaskInOrbit(degree, generators, baseOrdering)
         if nargin < 3
             baseOrdering = 1:degree;
         end
-        % simpler code:
-        % orbits = replab.Partition.permutationsOrbits(generators').blocks;
-        % mask1 = false(1, degree);
-        % for i = 1:length(orbits)
-        %     orbit = orbits{i};
-        %     [~, ind] = min(baseOrdering(orbit));
-        %     mask1(orbit(ind)) = true;
-        % end
         mask = false(1, degree);
         done = false(1, degree);
         i = 1;
@@ -39,5 +31,14 @@ function mask = minimalMaskInOrbit(degree, generators, baseOrdering)
             mask(minInd) = true;
             i = find(~done, 1);
         end
+        % simpler code: DEBUG
+        orbits = replab.Partition.permutationsOrbits(generators').blocks;
+        mask1 = false(1, degree);
+        for i = 1:length(orbits)
+            orbit = orbits{i};
+            [~, ind] = min(baseOrdering(orbit));
+            mask1(orbit(ind)) = true;
+        end
+        assert(isequal(mask, mask1));
     end
 end
