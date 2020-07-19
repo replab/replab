8e74051ea72d20b1feb017595e081c1428e02cf5
Denis Rosset <physics@denisrosset.com>
2020-07-19 09:20:05 -0400

    align uses Table
    See #339

--- src/+replab/+str/align.m
+++ src/+replab/+str/align.m
@@ -2,34 +2,15 @@ function s = align(table, spec)
 % Renders a table using the given LaTeX-inspired specification
 %
 % Args:
-%   table (cell array of charstring): Cell contents as strings, cell cannot be multiline
-%   spec (charstring): Contains as many 'l', 'c', or 'r' to prescribe alignement as there are columns
+%   table (cell(\*,\*) of charstring): Cell contents as strings, cell content cannot be multiline
+%   spec (char(1,\*)): Contains as many 'l', 'c', or 'r' to prescribe alignement as there are columns
 %
 % Returns:
-%   column vector of charstring: Formatted text lines
-    nR = size(table, 1);
-    nC = size(table, 2);
-    s = cell(nR, 1);
-    Wtable = cellfun(@(x) length(x), table);
-    W = max(Wtable, [], 1);
-    for r = 1:nR
-        row = char([]);
-        for c = 1:nC
-            el = table{r, c};
-            pad = W(c) - length(el);
-            switch spec(c)
-              case 'l'
-                lp = 0;
-                rp = pad;
-              case 'c'
-                lp = ceil(pad/2);
-                rp = pad - lp;
-              case 'r'
-                lp = pad;
-                rp = 0;
-            end
-            row = [row repmat(' ', [1 lp]) el repmat(' ', [1 rp])];
-        end
-        s{r} = row;
-    end
+%   (cell(\*,1) of charstring): Formatted text lines
+    T = replab.str.Table(table, 'colSep', '');
+    T.setAlign(1:T.nColumns, spec);
+    s = T.format(1e6, 1e6);
+    s = strsplit(s, '\n');
+    s = s(:);
+    s = cellfun(@deblank, s, 'uniform', 0);
 end
