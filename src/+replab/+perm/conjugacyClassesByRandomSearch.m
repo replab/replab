37a4f710d8b50495d96daa88860a30cbfd0d794a
Denis Rosset <physics@denisrosset.com>
2020-07-27 17:54:01 +0200

    Uniform conjugacy class sampling

--- src/+replab/+perm/conjugacyClassesByRandomSearch.m
+++ src/+replab/+perm/conjugacyClassesByRandomSearch.m
@@ -8,40 +8,35 @@ function classes = conjugacyClassesByRandomSearch(group)
 %
 % Returns:
 %   classImages (cell(1,\*) of `+replab.ConjugacyClass`): All conjugacy classes
-    classes = {};
-    remains = group.order;
     if group.order > 2^52
         factor = group.order/2^52;
     else
         factor = 1;
     end
     bar = replab.infra.repl.ProgressBar(group.order);
+    classes = {};
+    remains = group.order;
+    g = group.sample;
     while remains > 0
-        g = group.sample;
-        gn = g;
-        n = 1;
-        eo = double(group.elementOrder(g));
-        while n < eo
-            bar.step(group.order - remains);
-            newClass = true;
-            for i = 1:length(classes)
-                c = classes{i};
-                r = c.representative;
-                if isequal(replab.Permutation.cycleStructure(r), replab.Permutation.cycleStructure(gn))
-                    if c.contains(g)
-                        newClass = false;
-                        break
-                    end
+        bar.step(group.order - remains);
+        newClass = true;
+        for i = 1:length(classes)
+            c = classes{i};
+            r = c.representative;
+            if isequal(replab.Permutation.cycleStructure(r), replab.Permutation.cycleStructure(g))
+                if c.contains(g)
+                    newClass = false;
+                    break
                 end
             end
-            if newClass
-                c = replab.ConjugacyClass.make(group, gn);
-                classes{1,end+1} = c;
-                remains = remains - c.size;
-            end
-            gn = gn(g);
-            n = n + 1;
         end
+        if newClass
+            c = replab.ConjugacyClass.make(group, g);
+            classes{1,end+1} = c;
+            remains = remains - c.size;
+        end
+        g = c.representativeCentralizer.sample;
     end
+    assert(remains == 0);
     bar.finish;
 end
