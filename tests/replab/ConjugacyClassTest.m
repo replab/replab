e6bdc3327e2402013e1eb699ed05011986ddc215
Denis Rosset <physics@denisrosset.com>
2020-07-27 18:24:51 +0200

    More tests for conjugacy classes.

--- tests/replab/ConjugacyClassTest.m
+++ tests/replab/ConjugacyClassTest.m
@@ -18,3 +18,20 @@ function test_centralizer_order
         assert(size(cc1, 2) == cc2.size);
     end
 end
+
+function test_conjugacyClass_number
+    d = [5 6 7 8 9];
+    n = [7 11 15 22 30]; % partition function
+    if ReplabTestParameters.onlyFastTests
+        d = d(1:3);
+        n = n(1:3);
+    end
+    for i = 1:length(d)
+        S = replab.S(d(i));
+        C = S.conjugacyClasses;
+        sz = sum(cellfun(@(c) double(c.size), C));
+        assert(sz == S.order);
+        ni = length(S.conjugacyClasses);
+        assert(n(i) == ni);
+    end
+end
