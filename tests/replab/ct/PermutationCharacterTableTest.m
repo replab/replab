ba119a213eebbfc93e7e9e808f2e6574f8e7c1df
Joscelyn <joscelyn@vdveen.ca>
2020-08-05 11:03:40 -0400

    Tests for +ct subpackage functions

---
+++ tests/replab/ct/PermutationCharacterTableTest.m
@@ -0,0 +1,15 @@
+function test_suite = PermutationCharacterTableTest()
+    disp(['Setting up tests in ', mfilename()]);
+    try
+        test_functions = localfunctions();
+    catch
+    end
+    initTestSuite;
+end
+
+function test_petersen
+% Symmetric group of order 3
+    ct = replab.ct.PermutationCharacterTable(replab.S(3));
+    vals = [1,1,1;2,0,-1;1,-1,1];
+    assert(isequal(ct.characterValues, vals))
+end
