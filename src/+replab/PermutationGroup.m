4ea11cb141fe7e55d71d6c098f39ae59a4adc069
Denis Rosset <physics@denisrosset.com>
2020-08-13 11:48:30 +0200

    Computation of matrix stabilizers

--- src/+replab/PermutationGroup.m
+++ src/+replab/PermutationGroup.m
@@ -577,6 +577,24 @@ classdef PermutationGroup < replab.FiniteGroup
             sub = self.orderedPartitionStabilizer(partition);
         end
 
+        function sub = matrixStabilizer(self, matrix)
+        % Returns the permutation subgroup that leaves a given matrix invariant by joint permutation of its rows and columns
+        %
+        % Example:
+        %   >>> S4 = replab.S(4);
+        %   >>> A = [1 1 0 1; 1 1 1 0; 0 1 1 1; 1 0 1 1]; % adjacency matrix of the square
+        %   >>> G = S4.matrixStabilizer(A);
+        %   >>> G.order == 8 % is the dihedral group of order 8
+        %       1
+        %
+        % Args:
+        %   matrix (double(domainSize,domainSize)): Matrix to stabilize under permutation
+        %
+        % Returns:
+        %   `.PermutationGroup`: The subgroup of this group such that every element ``g`` satisfies ``matrix(g,g) == matrix``
+            sub = replab.bsgs.MatrixAutomorphism(self, matrix).subgroup;
+        end
+
         function s = setwiseStabilizer(self, set)
         % Returns the subgroup that stabilizes the given set as a set
         %
