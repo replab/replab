06802c5927399143de49731f5b9fd3b3b57e9247
Denis Rosset <physics@denisrosset.com>
2020-08-24 01:35:48 +0200

    Migrated tests

--- tests/replab/sym/SymmetricGroupIrrepsTest.m
+++ tests/replab/sym/SymmetricGroupIrrepsTest.m
@@ -12,18 +12,15 @@ function test_suite = SymmetricGroupIrrepsTest()
         youngs = {[1] [2] [1 1] [3] [2 1] [1 1 1] [4]};% [3 1] [2 2] [2 1 1] [1 1 1 1]}; % last part commented out as per issue #247
     end
 
-    if exist('syms')
-        for i = 1:length(youngs)
-            Y = youngs{i};
-            if i == 1
-                % This part of the test is currently commented for most i as of issue #247
-                rep = replab.sym.specht(Y);
-                test_suite = rep.laws.addTestCases(test_suite);
-                rep = replab.sym.youngSeminormal(Y);
-                test_suite = rep.laws.addTestCases(test_suite);
-            end
-            rep = replab.sym.youngOrthogonal(Y);
-            test_suite = rep.laws.addTestCases(test_suite);
-        end
+    for i = 1:length(youngs)
+        Y = youngs{i};
+        Sn = replab.S(sum(Y));
+        % This part of the test is currently commented for most i as of issue #247
+        rep = Sn.irrep(Y, 'specht');
+        test_suite = rep.laws.addTestCases(test_suite);
+        rep = Sn.irrep(Y, 'seminormal');
+        test_suite = rep.laws.addTestCases(test_suite);
+        rep = Sn.irrep(Y, 'orthogonal');
+        test_suite = rep.laws.addTestCases(test_suite);
     end
 end
