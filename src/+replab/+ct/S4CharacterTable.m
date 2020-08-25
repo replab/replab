513a5f41e7a2cb6bee629f7d72362af5945b32b2
Denis Rosset <physics@denisrosset.com>
2020-08-25 13:27:32 +0200

    Character tables for A4, A5, S4

---
+++ src/+replab/+ct/S4CharacterTable.m
@@ -0,0 +1,13 @@
+function ct = S4CharacterTable
+    G = replab.PermutationGroup.symmetric(4);
+    chars = replab.cyclotomic.fromStrings(...
+        {'1' '1' '1' '1' '1'
+         '1' '-1' '1' '1' '-1'
+         '2' '0' '2' '-1' '0'
+         '3' '-1' '-1' '0' '1'
+         '3' '1' '-1' '0' '-1'});
+    conjugacyClasses = {[1 2 3 4] [2 1 3 4] [2 1 4 3] [3 1 2 4] [4 1 2 3]};
+    classNames = {'1+1+1+1' '2+1+1' '2+2' '3+1' '4'};
+    classes = replab.ConjugacyClasses(G, cellfun(@(g) G.conjugacyClass(g), conjugacyClasses, 'uniform', 0));
+    ct = replab.CharacterTable(G, classes, chars, 'classNames', classNames);
+end
