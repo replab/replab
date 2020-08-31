02a4bb55b35318058d4497552047ae97afaadffb
Denis Rosset <physics@denisrosset.com>
2020-08-31 13:42:18 +0200

    More fixing for coverage

--- src/+replab/+ct/S4CharacterTable.m
+++ src/+replab/+ct/S4CharacterTable.m
@@ -1,9 +1,9 @@
 function ct = S4CharacterTable
     G = replab.PermutationGroup.symmetric(4);
-    mat = {'1' '1' '1' '1' '1'
-           '1' '-1' '1' '1' '-1'
-           '2' '0' '2' '-1' '0'
-           '3' '-1' '-1' '0' '1'
+    mat = {'1' '1' '1' '1' '1'; ...
+           '1' '-1' '1' '1' '-1'; ...
+           '2' '0' '2' '-1' '0'; ...
+           '3' '-1' '-1' '0' '1'; ...
            '3' '1' '-1' '0' '-1'};
     chars = replab.cyclotomic.fromStrings(mat);
     conjugacyClasses = {[1 2 3 4] [2 1 3 4] [2 1 4 3] [3 1 2 4] [4 1 2 3]};
