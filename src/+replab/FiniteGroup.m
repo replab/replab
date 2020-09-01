e84de9c93f8d39ab99f7d6e83c72e415deb94cc4
Denis Rosset <physics@denisrosset.com>
2020-09-01 22:12:16 +0200

    Compute group abelian invariants

--- src/+replab/FiniteGroup.m
+++ src/+replab/FiniteGroup.m
@@ -89,6 +89,44 @@ classdef FiniteGroup < replab.CompactGroup & replab.FiniteSet
             error('Abstract');
         end
 
+        function a = computeAbelianInvariants(self)
+        % See `.abelianInvariants`
+        %
+        % Inspired by GAP System "AbelianInvariants"
+            if self.isTrivial
+                a = zeros(1, 0);
+                return
+            end
+            a = zeros(1, 0);
+            G = self;
+            dsg = self.derivedSubgroup;
+            primeDivisors = unique(double(factor(self.order)));
+            for i = 1:length(primeDivisors)
+                p = primeDivisors(i);
+                ranks = zeros(1, 0);
+                r = 0;
+                while r ~= 1
+                    H = dsg;
+                    for j = 1:G.nGenerators
+                        H = H.closure(G.composeN(G.generator(j), p));
+                    end
+                    r = G.order / H.order;
+                    G = H;
+                    if r ~= 1
+                        ranks = [ranks length(factor(r))];
+                    end
+                end
+                if ~isempty(ranks)
+                    l = ones(1, ranks(1));
+                    for i = ranks
+                        l(1:i) = l(1:i) * p;
+                    end
+                    a = [a l];
+                end
+            end
+            a = sort(a);
+        end
+
         function e = computeExponent(self)
         % See `.exponent`
             eo = self.conjugacyClasses.classElementOrders;
@@ -167,6 +205,21 @@ classdef FiniteGroup < replab.CompactGroup & replab.FiniteSet
             e = self.cached('exponent', @() self.computeExponent);
         end
 
+        function a = abelianInvariants(self)
+        % Returns the group abelian invariants
+        %
+        % It computes the decomposition of the factor group of this group by its derived subgroup
+        % (which is abelian), and returns the primary decomposition of that abelian group.
+        %
+        % Example:
+        %   >>> G = replab.PermutationGroup.cyclic(100);
+        %   >>> isequal(G.abelianInvariants, [4 25])
+        %       1
+        %
+        % Returns:
+        %   integer(1,\*): Group abelian invariants
+            a = self.cached('abelianInvariants', @() self.computeAbelianInvariants);
+        end
 
         function c = conjugacyClass(self, g)
         % Returns the conjugacy class corresponding to the given element
