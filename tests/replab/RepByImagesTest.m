a9935470db307b74603128274b10b65fdf55d010
Denis Rosset <physics@denisrosset.com>
2020-11-02 15:57:58 -0500

    Remove all uses of the symbolic toolbox (#447)
    * replab.cyclotomic.uminus bugfix
    
    * Heterogenous replab.cyclotomic.{horzcat, vertcat}
    
    * Removed uses of the symbolic toolbox
    
    Fixes #444

--- tests/replab/RepByImagesTest.m
+++ tests/replab/RepByImagesTest.m
@@ -10,22 +10,22 @@ function test_suite = RepByImagesTest()
     sqrt1_2 = replab.cyclotomic.sqrtRational(1,2);
     img_a = [sqrt1_2 -sqrt1_2
              sqrt1_2 sqrt1_2];
-    img_x = [1 0; 0 -1];
+    img_x = replab.cyclotomic.fromDoubles([1 0; 0 -1]);
     rep = G.repByImages('R', 2, {img_a img_x});
     test_suite = rep.laws.addTestCases(test_suite);
 end
 
-function test_symbolic
+function test_cyclotomic
     S8 = replab.S(8); % we write D_16 as a subgroup of S_8
     a = [2 3 4 5 6 7 8 1]; % cyclic rotation
     x = [8 7 6 5 4 3 2 1]; % reflection
     D16 = S8.subgroup({a x});
-    s2 = sqrt(sym(2));
-    sigma_a = [1 -1; 1 1]/s2;
-    sigma_x = [1 0; 0 -1];
-    sigma = D16.repByImages('R', 2, {sigma_a sigma_x});
+    s2 = replab.cyclotomic.sqrtRational(1, 2);
+    sigma_a = replab.cyclotomic.sqrtRational(1, 2) * [1 -1; 1 1];
+    sigma_x = replab.cyclotomic.fromDoubles([1 0; 0 -1]);
+    sigma = D16.repByImages('R', 2, 'preimages', {a x}, 'images', {sigma_a sigma_x});
     img1 = sigma.image(S8.compose(a, x)); % should be [1 1; 1 -1]/sqrt(2)
-    img2 = [1 1; 1 -1]/s2;
+    img2 = replab.cyclotomic.sqrtRational(1, 2) * [1 1; 1 -1];
     assert(norm(double(img1) - double(img2)) == 0);
 end
 
