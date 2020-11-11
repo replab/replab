147b3fa59bdee70a286f9a0658b02632cf2a9f8d
Denis Rosset <physics@denisrosset.com>
2020-11-11 18:10:00 -0500

    Updated key/value pairs handling

---
+++ src/+replab/+util/keyValuePairsUpdate.m
@@ -0,0 +1,26 @@
+function [keyValuePairs exists oldValue] = keyValuePairsUpdate(keyValuePairs, key, newValue)
+% Updates a sequence of key/value pairs
+%
+% Args:
+%   keyValuePairs (cell(1,\*)): Key/value pairs (odd indices are charstring keys, even indices are values)
+%   key (charstring): Key to add or update
+%   newValue: Value to add or update
+%
+% Returns
+% -------
+%   keyValuePairs:
+%     cell(1,\*): Updated list of key/value pairs
+%   exists:
+%     logical: Whether the key was already present
+%   oldValue:
+%     Previous value if already present
+    [exists, ind] = ismember(key, keyValuePairs(1:2:end));
+    if exists
+        oldValue = keyValuePairs{ind+1};
+        keyValuePairs{ind+1} = newValue;
+    else
+        keyValuePairs{1,end+1} = key;
+        keyValuePairs{1,end+1} = newValue;
+        oldValue = [];
+    end
+end
