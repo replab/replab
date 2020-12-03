1c107864d8a666191ba04827d88994b7bd0cd449
Denis Rosset <physics@denisrosset.com>
2020-12-02 23:16:57 -0500

    Rep.computeIsUnitary

--- src/+replab/Rep.m
+++ src/+replab/Rep.m
@@ -461,7 +461,26 @@ classdef Rep < replab.Obj
         end
 
         function b = computeIsUnitary(self)
-            error('TODO');
+            if self.isExact && isa(self.group, 'replab.FiniteGroup')
+                for i = 1:self.group.nGenerators
+                    g = self.group.generator(i);
+                    I1 = self.image(g, 'exact');
+                    I2 = self.inverseImage(g, 'exact');
+                    if any(any(I1 ~= I2'))
+                        b = false;
+                        return
+                    end
+                end
+                b = true
+            else
+                [X err] = self.hermitianInvariant.project(speye(self.dimension), 'double');
+                if norm(X - speye(self.dimension), 'fro') > err
+                    b = false;
+                else
+                    warning('Setting isUnitary to false as a fallback value; it may be that a unitary exact representation is close enough too.');
+                    b = false;
+                end
+            end
         end
 
         function b = computeIsIrreducible(self)
