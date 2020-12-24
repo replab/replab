a82faacdaf357b346073f588f75bbe6caffc411c
Denis Rosset <physics@denisrosset.com>
2020-12-24 13:38:47 +0100

    replab.globals.verbosity doc

--- src/+replab/+globals/verbosity.m
+++ src/+replab/+globals/verbosity.m
@@ -1,8 +1,14 @@
 function value = verbosity(newValue)
 % Get/set the verbosity level
 %
+% The levels are understood as follows:
+%
+% - 1 is INFO, the standard log level indicating what is happening
+% - 2 is DEBUG, used for information that may be needed for diagnosing issues
+% - 3 is TRACE, the most fine-grained level of information, expect it to be very verbose
+%
 % Args:
-%   newValue (integer, optional): New value; if empty queries the global variable. 0 is silent, 1 is minimal, 2 shows diagnotics/iterations
+%   newValue (integer, optional): New value; if empty queries the global variable. 0 is silent, for 1/2/3 see list above
 %
 % Returns:
 %   integer: The stored value
