eee92bad36fa0b79457c1394356381a17198563c
Jean-Daniel Bancal <jdbancal@gmail.com>
2021-01-20 22:30:03 +0100

    Framework for functions written in another language

--- src/+replab/+graph/burningAlgorithmFast.m
+++ src/+replab/+graph/burningAlgorithmFast.m
@@ -21,71 +21,11 @@ function subsets = burningAlgorithmFast(edges)
 %     replab.graph.connectedComponents
 %     replab.graph.burningAlgorithm
 
-    persistent triedFastOption fastOptionWorks
-    if isempty(triedFastOption) || isempty(fastOptionWorks)
-        triedFastOption = false;
-        fastOptionWorks = false;
+    
+    persistent compiledInterface;
+    if isempty(compiledInterface)
+        compiledInterface = replab.dialects.Compiled('cpp');
     end
 
-    % If possible, we try to use the optimized code
-    if (~triedFastOption) || fastOptionWorks
-        firstPartWorks = true;
-        if ~triedFastOption
-            % We try to use the fast option
-            % Make sure we are in the current path
-            initialPath = pwd;
-            try
-                [pathStr, name, extension] = fileparts(which('replab_init'));
-                pathStr = strrep(pathStr, '\', '/');
-                pathStr = [pathStr, '/src/+replab/+graph'];
-                cd(pathStr)
-
-                needToCompile = true;
-                if (exist(['burningAlgorithmFast_mex.', mexext], 'file') == 2) || (exist(['burningAlgorithmFast_mex.', mexext], 'file') == 3)
-                    % If the program was already compiled, we check that the
-                    % compilation is up to date
-                    if exist(['burningAlgorithmFast_timestamp_', mexext, '.mat'], 'file')
-                        savedData = load(['burningAlgorithmFast_timestamp_', mexext, '.mat']);
-                        fileProperties = dir('burningAlgorithmFast_mex.cpp');
-                        if isequal(savedData.timestamp, fileProperties.date)
-                            needToCompile = false;
-                        end
-                    end
-                end
-
-                if needToCompile
-                    % If the program was never compiled, or the source was
-                    % modified, we try to compile it
-                    mex('-largeArrayDims','burningAlgorithmFast_mex.cpp')
-
-                    % We save the timestamp corresponding to this
-                    % compilation
-                    fileProperties = dir('burningAlgorithmFast_mex.cpp');
-                    timestamp = fileProperties.date;
-                    save(['burningAlgorithmFast_timestamp_', mexext, '.mat'], 'timestamp')
-                end
-            catch
-                firstPartWorks = false;
-            end
-            % return to the previous path
-            cd(initialPath);
-            triedFastOption = true;
-        end
-        if firstPartWorks
-            % The preparation worked, we try call the optimized method
-            fastOptionWorks = true;
-            try
-                subsets = replab.graph.burningAlgorithmFast_mex(edges);
-            catch
-                fastOptionWorks = false;
-            end
-        else
-            fastOptionWorks = false;
-        end
-    end
-
-    if ~fastOptionWorks
-        % Inform the dispatcher that this method did not succeed
-        subsets = replab.DispatchNext;
-    end
+    subsets = compiledInterface.call(edges);
 end
