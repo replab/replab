a4331008813523de6958de9fb9810fa23f0630f3
Denis Rosset <physics@denisrosset.com>
2021-02-03 17:21:05 +0100

    Working quaternion algebra recognition

--- src/+replab/+rep/canonicalEncoding_nonunitary.m
+++ src/+replab/+rep/canonicalEncoding_nonunitary.m
@@ -35,17 +35,22 @@ function subs = canonicalEncoding_nonunitary(sub, iterator)
         subs = {sub};
     elseif lambda > 0
         % pair of real-type representations
+        error('Not supported');
     else % lambda < 0
          % quaternion-type representation
         assert(mod(d, 4) == 0);
         [U1, D1] = eig(J, 'vector');
         [~, I1] = sort(real(D1));
         I1 = I1(:)';
+        % assert that we have (lambda, -lambda) pairs
         assert(all(D1(I1) + D1(fliplr(I1)) < tol));
+        % order the pairs
         I1 = [I1(1:d/4) fliplr(I1(d/4+1:end))];
+        % reorder everything and compute the left eigenvectors
         D1 = D1(I1);
         U1 = U1(:,I1);
         V1 = inv(U1);
+        % chop it off
         U2 = U1(:,1:d/4);
         V2 = V1(1:d/4,:);
         UU = blkdiag([U2 conj(U2)], [U2 conj(U2)]);
@@ -56,37 +61,23 @@ function subs = canonicalEncoding_nonunitary(sub, iterator)
         v = v/abs(v(1));
         w = sqrt(v);
         w = w(:).';
-        %w = [w 1i*w];
         W = diag([w 1i*w conj(w) -1i*conj(w)]);
         s1 = [1 0; 0 1];
         sy = [0 -1i; 1i 0];
-        P = [1:2:d/2 2:2:d/2 d/2+(1:2:d/2) d/2+(2:2:d/2)];
-        P = sparse(P(:)', 1:d, ones(1, d), d, d);
-        W1 = kron([s1-sy s1+sy; -s1-sy s1-sy]/2, eye(d/4));
-        XX = W1*P*conj(W) * VV*V*S *U*UU * W*P'*W1';
-        XX1 = W1*P*conj(W) * VV*V*iterator.next *U*UU * W*P'*W1';
-        %XX1 = diag([conj(w) w]) * VV*V* iterator.next *U*UU * diag([w conj(w)]);
-        % has the form [0 L; conj(L) 0] with L = blkdiag(L1, -L1)
-        error('asd');
-% $$$         Y = V2*V*S*U*U2;
-% $$$         D2 = ones(1, d);
-% $$$         for i = 2:d/2
-% $$$             f = Y(1,d/2+1)/Y(i,d/2+i);
-% $$$             D2(d/2+i) = f;
-% $$$         end
-% $$$         Y = diag(1./D2)*V2*V*S*U*U2*diag(D2);
-% $$$         f = sqrt(Y(d/2+1,1)/Y(1,d/2+1));
-% $$$         D2(d/2+1:end) = f * D2(d/2+1:end);
-% $$$         Y = diag(1./D2)*V2*V*S*U*U2*diag(D2);
-        sz = [1 0; 0 -1];
-        sx = [0 1; 1 0];
-        W11 = sz+1i*sx;
-        W12 = sz-1i*sx;
-        W21 = -1i*sz-sx;
-        W22 = -1i*sz+sx;
-        I = speye(d/4);
-        W = [kron(I, W11) kron(I, W12)
-             kron(I, W21) kron(I, W22)];
-% $$$         Y = W*diag(1./D2)*V2*V*S*U*U2*diag(D2)*W'
+        p = [1:d/4
+             d/4+1:d/2
+             d/2+1:3*d/4
+             3*d/4+1:d];
+        P = sparse(p(:)', 1:d, ones(1, d), d, d);
+        W1 = kron(eye(d/4), [s1-sy s1+sy; -s1-sy s1-sy]/2);
+        inj = U*UU * W*P*W1';
+        prj = W1*P'*conj(W) * VV*V;
+        assert(norm(imag(inj)) <= tol);
+        assert(norm(imag(prj)) <= tol);
+        inj = real(inj)/sqrt(2);
+        prj = real(prj)/sqrt(2);
+        XX = prj*S*inj;
+        XX1 = prj*iterator.next*inj;
+        subs = {sub.parent.subRep(inj, 'projection', prj, 'isIrreducible', true, 'frobeniusSchurIndicator', -2, 'isDivisionAlgebraCanonical', true)};
     end
 end
