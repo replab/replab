35e0bc02d994bc0a2fdc2f94e414f2a186369108
Denis Rosset <physics@denisrosset.com>
2021-02-25 08:56:07 +0100

    PermutationGroup.vectorFindLexMinimal: test case for identified bug

--- tests/replab/LexMinTest.m
+++ tests/replab/LexMinTest.m
@@ -30,3 +30,16 @@ function test_symmetric_group
         assert(all(smin == sort(s)));
     end
 end
+function test_issue_486
+    g1 = [7 6 5 9 4 2 1 10 3 8];
+    g2 = [7 6 9 5 3 2 8 1 4 10];
+    G = replab.PermutationGroup.of(g1, g2);
+    g = [7 2 4 3 9 6 1 10 5 8];
+    assert(G.contains(g));
+    s1 = [1 3 1 1 2 2 3 2 3 2];
+    s2 = [3 3 1 1 3 2 1 2 2 2];
+    assert(all(s2 == s1(g)));
+    r1 = G.vectorFindLexMinimal(s1);
+    r2 = G.vectorFindLexMinimal(s2);
+    assert(all(r1 == r2));
+end
