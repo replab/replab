6f21e947315c1b0a6abdd4729c747e7083adcb6d
Jean-Daniel Bancal <jdbancal@gmail.com>
2021-03-05 23:59:08 +0100

    Progress bar without time estimation

--- src/+replab/+infra/+repl/ProgressBar.m
+++ src/+replab/+infra/+repl/ProgressBar.m
@@ -69,6 +69,40 @@ classdef ProgressBar < handle
             self.consoleLine.update(str);
         end
 
+        function stepNoTimeEstimation(self, i, txt)
+        % Updates the progress bar with a 1-based index
+        %
+        % This method should be called at the beginning of the ``i``-th step.
+        % It prints the advancement but does not provide a time estimate
+        % for completion.
+        %
+        % Args:
+        %   i (integer): Current step
+        %   txt (integer, optional): Text corresponding to the current step
+            if nargin < 3
+                txt = '';
+            end
+            elapsed = etime(clock, self.startTime);
+            if (length(self.steps) >= 1) && (self.steps(end) >= i)
+                % Remove progresses larger than current one
+                lastSmaller = find(self.steps >= i, 1, 'first') - 1;
+                self.steps = self.steps(1:lastSmaller);
+                self.times = self.times(1:lastSmaller);
+            end
+            self.steps = [self.steps i];
+            self.times(1,end+1) = elapsed;
+            timeEst = '';
+            timeEstPad = repmat(' ', 1, self.largestTimeEstSize);
+            barFull = min(round(double(i)/double(self.n)*self.barSize), self.barSize);
+            barEmpty = self.barSize - barFull;
+            if ~isempty(txt)
+                txt = sprintf('   %s', txt);
+            end
+            str = sprintf('[%s%s] %s/%s, %s%s%s', repmat('#', 1, barFull), repmat('.', 1, barEmpty), ...
+                          strtrim(num2str(i)), strtrim(num2str(self.n)), timeEst, timeEstPad, txt);
+            self.consoleLine.update(str);
+        end
+
         function finish(self, finishStr)
         % Finishes the progress bar
         %
