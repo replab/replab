3ff54beaac6c6970f4636a725f250d19da4cf568
Denis Rosset <physics@denisrosset.com>
2021-03-10 11:27:51 +0100

    Working isotypic construction code

---
+++ src/+replab/+irreducible/isotypicComponent.m
@@ -0,0 +1,41 @@
+function iso = isotypicComponent(rep, irrep, type)
+% Returns the isotypic component corresponding to a given irrep
+%
+% Based on original code by Sotiris Mygdalas, summer 2020
+%
+% Args:
+%   rep (`+replab.Rep`): Representation to find the isotypic component of
+%   irrep (`+replab.Rep`): Irreducible representation
+%   type ('exact', 'double', 'double/sparse'): Type of the projector to compute
+%
+% Returns:
+%   `+replab.Isotypic`: Isotypic component, may be empty
+    if nargin < 3 || isempty(type)
+        type = 'double';
+    end
+    assert(rep.overC); % can we relax this?
+    T = replab.irreducible.serreTensor(rep, irrep, type);
+    proj1 = T(:,:,1,1);
+    [~, pivot] = rref(double(proj1).');
+    m = length(pivot);
+    D = rep.dimension;
+    d = irrep.dimension;
+    if strcmp(type, 'exact')
+        I = replab.cyclotomic.zeros(D, m, d);
+    else
+        I = zeros(D, m, d);
+    end
+    I(:,:,1) = T(:,pivot,1,1);
+    for i = 2:d
+        I(:,:,i) = T(:,:,i,1) * T(:,pivot,1,1);
+    end
+    I = permute(I, [1 3 2]);
+    sub = rep.subRep(I);
+    Piso = sub.projection(type);
+    P = permute(reshape(Piso, [d m D]), [1 3 2]);
+    irreps = cell(1, m);
+    for i = 1:m
+        irreps{i} = rep.subRep(I(:,:,i), 'projection', P(:,:,i), 'isIrreducible', true, 'isUnitary', irrep.isUnitary);
+    end
+    iso = replab.Isotypic(rep, irreps, irrep, Piso);
+end
--- src/+replab/+irreducible/serreTensor.m
+++ src/+replab/+irreducible/serreTensor.m
@@ -1,5 +1,7 @@
-function [T, P, n] = serreTensor(rep, irrep, type)
+function [T, P] = serreTensor(rep, irrep, type)
 % Returns, for a given representation, the tensor encoding the status of an irrep in a representation
+%
+% Based on original code by Sotiris Mygdalas, summer 2020
 %
 % Args:
 %   rep (`+replab.Rep`): Representation of dimension ``D``
@@ -12,9 +14,11 @@ function [T, P, n] = serreTensor(rep, irrep, type)
 %     Serre tensor
 %   P: double(D,D) or `+replab.cyclotomic`(D,D), may be sparse
 %     Projector into the isotypic component
-%   n: integer
-%     Returns the multiplicity of the given irreducible character
+    if nargin < 3 || isempty(type)
+        type = 'double';
+    end
     assert(isa(rep, 'replab.Rep'));
+    assert(isa(irrep, 'replab.Rep'));
     assert(rep.field == irrep.field);
     assert(rep.group == irrep.group);
     assert(irrep.isIrreducible);
@@ -22,18 +26,12 @@ function [T, P, n] = serreTensor(rep, irrep, type)
     d = irrep.dimension;
     t = rep.group.trivialRep(rep.field, d*D);
     K = kron(dual(irrep), rep);
-    if nargin < 3 || isempty(type)
-        type = 'double';
-    end
     if strcmp(type, 'exact');
         T = K.equivariantFrom(t).project(replab.cyclotomic.eye(d*D), 'exact');
     else
         T = K.equivariantFrom(t).project(eye(d*D), 'double');
     end
-    T = permute(reshape(T, [D d D d]), [1 3 2 4]);
+    T = permute(reshape(T, [D d D d]), [1 3 2 4]) * irrep.dimension;
     P = reshape(T, [D*D d*d]) * reshape(eye(d), [d*d 1]);
     P = reshape(P, [D D]);
-    n = trace(P)/trace(P*P);
-    T = T * n;
-    P = P * n;
 end
