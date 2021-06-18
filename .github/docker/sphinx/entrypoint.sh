c80516a82db6d9e3ec6eb81ebc5125921448cd16
Jean-Daniel Bancal <jdbancal@gmail.com>
2021-06-18 13:34:22 +0200

    Disctinct workflows

--- .github/docker/sphinx/entrypoint.sh
+++ .github/docker/sphinx/entrypoint.sh
@@ -11,13 +11,11 @@ ls -al
 
 git rev-parse HEAD
 
-
+# Prepare doc folder and install python requirements
 mkdir docs
 pip3 install -r sphinx/requirements.txt
-echo "Here indeed"
 
-
-
+# prepare commands
 export ADDPATH_COMMAND="replab_init('verbose', 2);"
 GENERATE_COMMAND="exit(~replab_generate('sphinx'));";
 echo "GENERATE_COMMAND=$GENERATE_COMMAND";
@@ -28,13 +26,12 @@ octave -q --eval "ver"
 # Check that octave can access java
 octave --eval "b = javaMethod('valueOf', 'java.math.BigInteger', 2)"
 
-
-# Run command
+# Run commands
 if octave -q --eval "$ADDPATH_COMMAND $GENERATE_COMMAND"; then
   # Check where we ended up and what's going on where we are
   pwd
   ls -alh
 else
-  # The command failed
+  # The commands failed
   exit 1
 fi
