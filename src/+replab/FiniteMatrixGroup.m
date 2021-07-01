958e92c5f53711028acc88c3f09abe7d25ee407d
Denis Rosset <physics@denisrosset.com>
2021-07-01 13:01:24 +0200

    Starting finite matrix group support

--- src/+replab/FiniteMatrixGroup.m
+++ src/+replab/FiniteMatrixGroup.m
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 classdef FiniteMatrixGroup < replab.NiceFiniteGroup
 
     properties (SetAccess = protected)
@@ -7,10 +8,18 @@ classdef FiniteMatrixGroup < replab.NiceFiniteGroup
 
     properties (Access = protected)
         permutationImages % (integer(\*,\*)): Each row gives the permutation representation of an element in the regular representation
+=======
+classdef FiniteMatrixGroup < replab.FiniteGroup
+
+    properties (SetAccess = protected)
+        matrixSize % (integer): Matrix size
+        elements % (cyclo.CycloSet): List of elements (Java object)
+>>>>>>> Starting finite matrix group support
     end
 
     methods
 
+<<<<<<< HEAD
         function self = FiniteMatrixGroup(matrixSize, generators, varargin)
             generators = cellfun(@(g) replab.cyclotomic(g), generators, 'uniform', 0);
             args = struct('relators', []);
@@ -22,12 +31,17 @@ classdef FiniteMatrixGroup < replab.NiceFiniteGroup
             self.elementSet = replab.matrix.dimino(generators, identity);
             order = self.elementSet.nElements;
             self.permutationImages = zeros(order, order);
+=======
+        function self = FiniteMatrixGroup(matrixSize, generators)
+            self.identity = replab.cyclotomic.eye(matrixSize);
+>>>>>>> Starting finite matrix group support
         end
 
     end
 
     methods % Implementations
 
+<<<<<<< HEAD
         % Domain
 
         function l = eqv(self, x, y)
@@ -46,6 +60,8 @@ classdef FiniteMatrixGroup < replab.NiceFiniteGroup
 
         % Group
 
+=======
+>>>>>>> Starting finite matrix group support
         function xInv = inverse(self, x)
             xInv = inv(x);
         end
