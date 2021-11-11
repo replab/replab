912f2dda9ccf68eaa5393f3bdbb7c68dce676733
Jean-Daniel Bancal <jdbancal@gmail.com>
2021-11-11 13:15:14 +0100

    Generating a code coverage result with matlab

--- src/replab_runtests.m
+++ src/replab_runtests.m
@@ -99,20 +99,32 @@ function result = replab_runtests(varargin)
 
     % calls the relevant test suite
     if args.withCoverage
-        % Here are the files patterns we don't want to include in the
-        % coverage monitoring. These are checked by MOcov individually in
-        % each subdirectory.
-        patternsToExclude = {'replab_*.m', 'callOriginalHelp.m'};
-
-        % We define the test command
-        command = 'moxunit_runtests(''tests/codeCoverageHelperFunction.m'', ''-verbose'', ''-with_coverage'', ''-cover'', ''src'', ''-cover_json_file'', ''coverage.json'', ''-cover_xml_file'', ''coverage.xml'', ''-cover_html_dir'', ''coverage_html''';
-        for i = 1:numel(patternsToExclude)
-            command = [command, ', ''-cover_exclude'', ''', patternsToExclude{i}, ''''];
+        try
+            addpath('tests');
+            % Set parameters
+            matlabTestSuite.slowtests(args.slowtests);
+            matlabTestSuite.doctests(args.doctests);
+            matlabTestSuite.notebooks(args.notebooks);
+            % Launch tests
+            result = matlabLaunchTestCovering;
+        catch
+            result = false;
         end
-        command = [command, ')']
-
-        % and call it
-        result = eval(command);
+        rmpath('tests');
+%         % Here are the files patterns we don't want to include in the
+%         % coverage monitoring. These are checked by MOcov individually in
+%         % each subdirectory.
+%         patternsToExclude = {'replab_*.m', 'callOriginalHelp.m'};
+% 
+%         % We define the test command
+%         command = 'moxunit_runtests(''tests/codeCoverageHelperFunction.m'', ''-verbose'', ''-with_coverage'', ''-cover'', ''src'', ''-cover_json_file'', ''coverage.json'', ''-cover_xml_file'', ''coverage.xml'', ''-cover_html_dir'', ''coverage_html''';
+%         for i = 1:numel(patternsToExclude)
+%             command = [command, ', ''-cover_exclude'', ''', patternsToExclude{i}, ''''];
+%         end
+%         command = [command, ')']
+% 
+%         % and call it
+%         result = eval(command);
     else
         result = moxunit_runtests('tests', '-verbose', '-recursive');
     end
