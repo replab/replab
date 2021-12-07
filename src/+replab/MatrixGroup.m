2323f329a16deb0956606c7296dfa303957b969f
Denis Rosset <physics@denisrosset.com>
2021-12-07 02:58:30 +0100

    MatrixGroup.naturalRep

--- src/+replab/MatrixGroup.m
+++ src/+replab/MatrixGroup.m
@@ -23,9 +23,42 @@ classdef MatrixGroup < replab.gen.FiniteGroup
             else
                 type = args.type;
             end
+            generators = cellfun(@(g) replab.cyclotomic(g), generators, 'uniform', 0);
             self@replab.gen.FiniteGroup(type, generators, rest{:});
         end
 
     end
+
+    methods % Representations
+
+        function rep = naturalRep(self, varargin)
+        % Returns the natural representation of this matrix group
+        %
+        % The natural representation ``rho`` has images ``rho(g) = g``.
+        %
+        % Example:
+        %   >>> M = replab.MatrixGroup(2, {[0 1; 1 0], '[1/sqrt(2), 1/sqrt(2); -1/sqrt(2), 1/sqrt(2)]'}); % docttest: +cyclotomic
+        %   >>> M.naturalRep.isIrreducible
+        %       1
+        %
+        % Keyword Args:
+        %   field ({'R' or 'C'}): Field over which to define the representation, default: 'C'
+        %
+        % Returns:
+        %   `.Rep`: The natural representation of this group
+            args = struct('field', 'C');
+            args = replab.util.populateStruct(args, varargin);
+            switch args.field
+                case 'R'
+                  assert(all(cellfun(@(g) isreal(g), self.generators)), 'All generators must be real');
+              case 'C'
+              otherwise
+                error('Invalid field %s', args.field);
+            end
+            rep = self.repByImages(args.field, self.type.d, 'preimages', self.generators, 'images', self.generators);
+        end
+
+    end
+
 
 end
