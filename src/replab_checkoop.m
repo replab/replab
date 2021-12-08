342c9289492f8ad50367a97dd5004a9402178fed
Jean-Daniel Bancal <jean-daniel.bancal@ipht.fr>
2021-12-08 11:07:51 +0100

    Improved display for easier access to relevant code parts

--- src/replab_checkoop.m
+++ src/replab_checkoop.m
@@ -1,16 +1,26 @@
-function ok = replab_checkoverloading
-% Checks that inheritance of methods is always well-defined
+function ok = replab_checkoop
+% Checks the object oriented programming structure of the code
 %
-% This includes two kinds of checks:
+% The main reason for this function is the fact that inheritance rules are
+% a bit different between Matlab and Octave.
 %
-% 1. When inheriting from multiple classes, a given method can be provided
+% This includes three kinds of checks, performed one after the other one
+% upon success:
+%
+% 1. Check that inherited methods admit only one possible implementation.
+% When inheriting from multiple classes, a given method can be provided
 % several definitions. Since Matlab and Octave use different rules to
-% choose which definition to use, we check here that no such situation
-% arises (this is needed to guarantee that the same code is provided to
-% both interpreters)
+% choose which definition to use (and some of these rules might evolve in
+% the future), we check here that no such situation arises. This is needed
+% to guarantee that the same code is provided to both interpreters.
 %
 % 2. When the first test passes, this function also checks that all classes
-% which are not inherited by any other class contain no 'Abstract' method.
+% which are not inherited by any other class contain no 'Abstract' method,
+% i.e. that no top class is abstract.
+%
+% 3. Finally, we check that all references to replab objects in the code
+% point to an existing definition (this is similar to what replab_checkhelp
+% does for the documentation).
 %
 % Returns:
 %   logical: True if no problems were detected
@@ -33,7 +43,7 @@ function ok = replab_checkoverloading
     for i = 1:length(ac)
         cl = ac{i};
         if length(cl.ownSuperclasses) >= 2
-            disp([cl.fullIdentifier, ' inherits from ', num2str(length(cl.ownSuperclasses)), ' other classes.']);
+            disp([replab.infra.repl.linkOpen(cl.fullIdentifier, cl.fullIdentifier, cl.absoluteFilename, 1), ' inherits from ', num2str(length(cl.ownSuperclasses)), ' other classes.']);
             % This class inherits from several classes. We check that all
             % its methods are uniquely defined
             mts = cl.allMethods;
@@ -80,7 +90,7 @@ function ok = replab_checkoverloading
                     assert(min(implementations) > 0, 'Found an implementation that was not registred in the codebase');
                     if min(implementations) ~= max(implementations)
                         nbAmbiguousMethods = nbAmbiguousMethods + 1;
-                        disp(['Method ', replab.infra.repl.linkOpen(mts{j}.fullIdentifier, mts{j}.fullIdentifier, cl.absoluteFilename, 1), ' admits ', num2str(length(unique(implementations))), ' declarations:']);
+                        disp(['Method ', mts{j}.fullIdentifier, ' admits ', num2str(length(unique(implementations))), ' declarations:']);
                         for k = unique(implementations)
                             disp(['  - in ', replab.infra.repl.linkOpen(decl{k}.fullIdentifier, decl{k}.fullIdentifier, decl{k}.absoluteFilename, decl{k}.startLineNumber)]);
                         end
@@ -92,11 +102,13 @@ function ok = replab_checkoverloading
     end
 
     ok = (nbAmbiguousMethods == 0);
-    if ~ok
+    if ok
+        disp('No ambiguous method overloading encountered.')
+    else
         warning([num2str(nbAmbiguousMethods), ' methods were found who could be ambiguous.']);
+        return;
     end
     
-    return;
     
     
 end
